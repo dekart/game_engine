c96083f5a585519ca2a8f035d4126c071e566530
DEkart <dekart@Lamptop.(none)>
2009-04-13 23:10:14 +0600

    Fix template for publisher

--- lib/publisher/mission.rb
+++ lib/publisher/mission.rb
@@ -8,10 +8,10 @@ module Publisher
     include FacebookHelper
 
     def completed_template
-      one_line_story_template "{*actor*} completed \"{*mission_name*}\" mission in #{link_to(fb_app_name, "{*mission_url*}")} game!"
+      one_line_story_template "{*actor*} completed \"{*mission*}\" mission in #{link_to(fb_app_name, root_url(:canvas => true))} game!"
       short_story_template(
-        "{*actor*} received \"{*title*}\" title in #{link_to(fb_app_name, "{*mission_url*}")} game!",
-        "{**} completed \"{*mission*}\" mission and received \"{*title*}\" title in #{link_to(fb_app_name, "{*mission_url*}")} game."
+        "{*actor*} received \"{*title*}\" title in #{link_to(fb_app_name, root_url(:canvas => true))} game!",
+        "{*actor*} completed \"{*mission*}\" mission and received \"{*title*}\" title in #{link_to(fb_app_name, root_url(:canvas => true))} game."
       )
     end
   end
