1a9e1e39cea8ec52828c1b8fbd8d9b782cc1d348
DEkart <dekart@Lamptop.(none)>
2009-04-15 01:21:17 +0600

    Work on invitations

--- app/helpers/characters_helper.rb
+++ app/helpers/characters_helper.rb
@@ -1,5 +1,31 @@
 module CharactersHelper
-  def character_name(character, options = {})
+  def character_name(character_or_user, options = {})
+    character = character_for(character_or_user)
+
     character.name.blank? ? fb_name(character.user, {:linked => false}.merge(options)) : character.name
+  end
+
+  def character_picture(character_or_user, options = {})
+    character = character_for(character_or_user)
+
+    fb_profile_pic(character.user, {:linked => false}.merge(options))
+  end
+
+  def character_name_link(character_or_user, link_options = {}, name_options = {})
+    character = character_for(character_or_user)
+
+    link_to(character_name(character, name_options), character_url(character, :canvas => true), link_options)
+  end
+
+  def character_picture_link(character_or_user, link_options = {}, picture_options = {})
+    character = character_for(character_or_user)
+
+    link_to(character_picture(character, picture_options), character_url(character, :canvas => true), link_options)
+  end
+
+  protected
+
+  def character_for(character_or_user)
+    character_or_user.is_a?(Character) ? character_or_user : character_or_user.character
   end
 end
