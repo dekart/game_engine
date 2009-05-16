1c43b083cec0fd571ef76d5db71e1a18be9951be
DEkart <dekart@Lamptop.(none)>
2009-05-16 11:39:05 +0600

    Adding correct links to notifications and feed stories

--- lib/publisher/fight.rb
+++ lib/publisher/fight.rb
@@ -11,13 +11,13 @@ module Publisher
       send_as :notification
       recipients fight.victim.user
       from user
-      fbml "attacked you in <b><fb:application-name /></b> game and #{fight.attacker_won? ? "WON" : "LOST"}!"
+      fbml "attacked you in #{link_to(fb_app_name(:linked => false), root_url)} game and #{fight.attacker_won? ? "WON" : "LOST"}!  #{link_to("Fight Back &raquo;", character_url(fight.attacker))}"
     end
 
     def attack_template
-      one_line_story_template "{*actor*} attacked {*victim*} in #{link_to(fb_app_name, root_url(:canvas => true))} game and WON the battle!"
+      one_line_story_template "{*actor*} attacked {*victim*} in #{link_to(fb_app_name(:linked => false), root_url)} game and WON the battle!"
       short_story_template(
-        "{*actor*} attacked {*victim*} in #{link_to(fb_app_name, root_url(:canvas => true))} game and WON the battle!",
+        "{*actor*} attacked {*victim*} in #{link_to(fb_app_name(:linked => false), root_url)} game and WON the battle!",
         "{*actor*} received +{*money*} money, +{*experience*} experience, and lost {*attacker_hp_loss*} health points.
          {*victim*} received {*victim_hp_loss*} points of damage"
       )
