240133d3daefc183c9e653b0113d4624740799f9
DEkart <dekart@Lamptop.(none)>
2009-05-19 00:41:14 +0600

    Rework restorable attributes to make them more configurable and clean

--- lib/restorable_attribute.rb
+++ lib/restorable_attribute.rb
@@ -1,14 +1,44 @@
 module RestorableAttribute
-  def restorable_attribute(name, limit, restore_period)
-    define_method("#{name}_updated_at") do
-      self["#{name}_updated_at"] || Time.now
+  class Base
+    def initialize(name, container, options = {})
+      @name = name
+      @container = container
+      @options = options
     end
 
-    define_method(name) do
-      calculated_value = self[name] + (Time.now - self.send("#{name}_updated_at")).to_i / restore_period
+    def updated_at
+      @container["#{@name}_updated_at"] || Time.now
+    end
 
-      if calculated_value > self.send(limit)
-        return self.send(limit)
+    def restore_rate
+      if @options[:restore_rate].is_a?(Symbol)
+        @container.send(@options[:restore_rate])
+      else
+        @options[:restore_rate] || 1
+      end
+    end
+
+    def restore_period
+      if @options[:restore_period].is_a?(Symbol)
+        @container.send(@options[:restore_period])
+      else
+        @options[:restore_period] || 5.minutes
+      end
+    end
+
+    def limit
+      @options[:limit] ? @container.send(@options[:limit]) : 1_000_000_000_000
+    end
+
+    def stored_value
+      @container[@name]
+    end
+
+    def value
+      calculated_value = self.stored_value + self.restore_rate * self.restores_since_last_update
+
+      if calculated_value > self.limit
+        return self.limit
       elsif calculated_value < 0
         return 0
       else
@@ -16,24 +46,57 @@ module RestorableAttribute
       end
     end
 
-    define_method("#{name}=") do |value|
-      compensation = restore_period - self.send("time_to_#{name}_restore")
-      self[name] = value
-      self.send("#{name}_updated_at=", Time.now - compensation)
+    def restores_since_last_update
+      (Time.now - self.updated_at).to_i / self.restore_period
     end
 
-    define_method("#{name}_restore_time") do |restore_to|
-      restore_to = self.send(limit) if restore_to > self.send(limit)
+    def value=(value)
+      @container[@name] = value
 
-      if self.send(name) >= restore_to
+      @container.send("#{@name}_updated_at=", Time.now - self.restore_period + self.time_to_restore)
+    end
+
+    def restore_time(restore_to)
+      restore_to = self.limit if restore_to > self.limit
+
+      if self.value >= restore_to
         return 0
       else
-        (self.send("#{name}_updated_at") + (restore_to - self[name]) * restore_period - Time.now).to_i
+        (self.updated_at + ((restore_to - self.stored_value).to_f / self.restore_rate).ceil * self.restore_period - Time.now).to_i
       end
     end
 
+    def time_to_restore
+      return 0 if self.value == self.limit
+      
+      self.restore_period - (Time.now - self.updated_at).to_i % self.restore_period.to_i
+    end
+  end
+
+  def restorable_attribute(name, options = {})
+    define_method("#{name}_restorable") do
+      @restorable_attributes ||= {}
+      @restorable_attributes[name] ||= Base.new(name, self, options)
+    end
+
+    define_method("#{name}_updated_at") do
+      send("#{name}_restorable").updated_at
+    end
+
+    define_method(name) do
+      send("#{name}_restorable").value
+    end
+
+    define_method("#{name}=") do |value|
+      send("#{name}_restorable").value = value
+    end
+
+    define_method("#{name}_restore_time") do |restore_to|
+      send("#{name}_restorable").restore_time(restore_to)
+    end
+
     define_method("time_to_#{name}_restore") do
-      send("#{name}_restore_time", self.send(name) + 1)
+      send("#{name}_restorable").time_to_restore
     end
   end
 end
