e702b81da275a71b2c1abca9a3df8f22d6139230
DEkart <dekart@Lamptop.(none)>
2009-05-19 01:55:01 +0600

    Changing formatting of fight descriptions

--- app/helpers/fights_helper.rb
+++ app/helpers/fights_helper.rb
@@ -4,10 +4,10 @@ module FightsHelper
 
     text << fb_it(:attacker, link_to(character_name(fight.attacker), fight.attacker))
     text << fb_it(:victim, link_to(character_name(fight.victim), fight.victim))
-    text << fb_it(:money, content_tag(:span, fight.money, :class => :money))
-    text << fb_it(:experience_points, fight.experience)
-    text << fb_it(:attacker_damage, fight.attacker_hp_loss)
-    text << fb_it(:victim_damage, fight.victim_hp_loss)
+    text << fb_it(:money, content_tag(:span, fight.money, :class => "attr basic_money"))
+    text << fb_it(:experience_points, content_tag(:span, fight.experience, :class => "attr experience"))
+    text << fb_it(:attacker_damage, content_tag(:span, fight.attacker_hp_loss, :class => "attr health"))
+    text << fb_it(:victim_damage, content_tag(:span, fight.victim_hp_loss, :class => "attr health"))
 
     return fb_i(text)
   end
