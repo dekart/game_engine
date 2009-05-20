06251eae82caaf89339acd1caa092506d2707652
DEkart <dekart@Lamptop.(none)>
2009-05-20 21:57:52 +0600

    Separate page for buying VIP money

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => "characters", :action => "index"
 
+  map.buy_vip_money "/characters/buy_vip_money", :controller => "characters", :action => "buy_vip_money"
+
   map.resources(:users,
     :collection => { :invite => :any },
     :member => {
