cfa95233cdb6f4fb2edd9dc5295be38f40081604
DEkart <dekart@Lamptop.(none)>
2009-06-12 19:48:39 +0600

    Styling mission requirements

--- app/helpers/missions_helper.rb
+++ app/helpers/missions_helper.rb
@@ -11,7 +11,11 @@ module MissionsHelper
         case requirement.name
         when "item"
           result << content_tag(:div,
-            image_tag(requirement.item.image.url(:icon)),
+            fb_tag(:img,
+              fb_ta(:alt, fb_i(requirement.item.name)) +
+              fb_ta(:title, fb_i(requirement.item.name)) +
+              fb_ta(:src, image_path(requirement.item.image.url(:icon)))
+            ),
             :class => "item" + (" not_satisfied" unless requirement.satisfies?(current_character))
           )
         end
