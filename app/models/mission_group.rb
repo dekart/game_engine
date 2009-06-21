1cebfffdb134401293610e16b21a9eea6d09f1ad
DEkart <dekart@Lamptop.(none)>
2009-06-22 00:36:36 +0600

    Display next mission group in mission list

--- app/models/mission_group.rb
+++ app/models/mission_group.rb
@@ -1,6 +1,12 @@
 class MissionGroup < ActiveRecord::Base
   has_many :missions, :dependent => :nullify
 
+  named_scope :next_for, Proc.new{|character|
+    {
+      :conditions => ["level > ?", character.level],
+      :order      => :level
+    }
+  }
   validates_presence_of :name, :level
   validates_numericality_of :level, :allow_nil => true
 end
