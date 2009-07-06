87f722b777f028c2c8a1a5d75f2a79fab66a23c0
DEkart <dekart@Lamptop.(none)>
2009-07-07 01:01:16 +0600

    Display users per 24 hours in stats

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -1,6 +1,7 @@
 class Admin::StatisticsController < ApplicationController
   def index
-    @total_users = User.count
-    @latest_users = User.find(:all, :order => "created_at DESC", :limit => 10)
+    @total_users    = User.count
+    @last_day_users = User.count(:conditions => ["created_at >= ?", 24.hours.ago])
+    @latest_users   = User.find(:all, :order => "created_at DESC", :limit => 5)
   end
 end
