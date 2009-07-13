ecc420415dccbf673d175608a126553b3bed1b41
DEkart <dekart@Lamptop.(none)>
2009-07-14 00:55:48 +0600

    Fix Use button labels

--- app/helpers/inventories_helper.rb
+++ app/helpers/inventories_helper.rb
@@ -39,11 +39,11 @@ module InventoriesHelper
 
   def inventory_use_label(inventory)
     limit_label = if inventory.usage_limit.nil?
-      fb_i(t("inventories.index.buttons.use.unlimited"))
+      fb_i(t("inventories.use.button.unlimited"))
     elsif inventory.usage_limit > 1
-      fb_i(t("inventories.index.buttons.use.limited", :left => inventory.uses_left, :limit => inventory.usage_limit))
+      fb_i(t("inventories.use.button.limited", :left => inventory.uses_left, :limit => inventory.usage_limit))
     end
 
-    fb_i(t("inventories.index.buttons.use.base") + fb_it(:usage_limit,  limit_label ))
+    fb_i(t("inventories.use.button.base") + fb_it(:usage_limit,  limit_label ))
   end
 end
