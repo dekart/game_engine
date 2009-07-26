7c8b5dbf3776538a21ad1e87a966a36b97734834
DEkart <dekart@Lamptop.(none)>
2009-07-27 01:04:10 +0600

    Include stylesheet instead of linking when trying it

--- app/helpers/stylesheets_helper.rb
+++ app/helpers/stylesheets_helper.rb
@@ -1,22 +1,28 @@
 module StylesheetsHelper
   def editable_stylesheet_link
-    style = params[:try_stylesheet] ? Stylesheet.find(params[:try_stylesheet]) : Stylesheet.find_by_current(true)
-
-    if Rails.env.development?
-      content_tag(:style, 
-        format_stylesheet_content(style ? style.content : File.read(default_stylesheet_path))
+    if params[:try_stylesheet] and style = Stylesheet.find_by_id(params[:try_stylesheet])
+      return content_tag(:style,
+        format_stylesheet_content(style.content)
       )
     else
-      if style
-        stylesheet_link_tag(
-          stylesheet_url(style,
-            :format     => :css,
-            :updated_at => style.updated_at,
-            :canvas     => false
-          )
+      style = Stylesheet.find_by_current(true)
+
+      if Rails.env.development?
+        content_tag(:style,
+          format_stylesheet_content(style ? style.content : File.read(default_stylesheet_path))
         )
       else
-        stylesheet_link_tag("default.css?#{File.mtime(default_stylesheet_path)}")
+        if style
+          stylesheet_link_tag(
+            stylesheet_url(style,
+              :format     => :css,
+              :updated_at => style.updated_at,
+              :canvas     => false
+            )
+          )
+        else
+          stylesheet_link_tag("default.css?#{File.mtime(default_stylesheet_path)}")
+        end
       end
     end
   end
