af59bc52d92be3daa9e24e49d4710c61cbfc63b0
DEkart <dekart@Lamptop.(none)>
2009-08-11 01:45:37 +0600

    Refactor VIP items list

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,11 +2,11 @@ class ItemsController < ApplicationController
   def index
     @item_group = parents.item_group || ItemGroup.first(:order => :position)
 
-    @basic_items = @item_group.items.basic.available_in(:shop).available_for(current_character).paginate(
+    @items = @item_group.items.basic.available_in(:shop).available_for(current_character).paginate(
       :page     => params[:page],
       :per_page => 10
     )
     
-    @vip_items = @item_group.items.vip.available_in(:shop).available_for(current_character)
+    @special_item = @item_group.items.vip.available_in(:shop).available_for(current_character).first(:order => "RAND()")
   end
 end
