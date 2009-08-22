c09a30cc605d15196334c978b61df6385baeb213
DEkart <dekart@Lamptop.(none)>
2009-08-22 18:33:49 +0600

    Disable tutorial for existing users

--- db/migrate/20090820183834_add_skip_tutorial_to_users.rb
+++ db/migrate/20090820183834_add_skip_tutorial_to_users.rb
@@ -1,6 +1,8 @@
 class AddSkipTutorialToUsers < ActiveRecord::Migration
   def self.up
     add_column :users, :skip_tutorial, :boolean
+
+    User.update_all("skip_tutorial = 1")
   end
 
   def self.down
