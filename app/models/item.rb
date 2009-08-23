e05d7d37d0d730605f4324fb3ac2adb9a4c5396d
DEkart <dekart@Lamptop.(none)>
2009-08-23 23:52:31 +0600

    Rename quest availability group back to mission

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ActiveRecord::Base
-  AVAILABILITIES = [:shop, :special, :loot, :quest]
+  AVAILABILITIES = [:shop, :special, :loot, :mission]
 
   belongs_to  :item_group
   has_many    :inventories
