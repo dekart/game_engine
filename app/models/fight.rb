644b9ecf6b7cc45c2a10146abb83873388497c67
DEkart <dekart@Lamptop.(none)>
2009-08-24 01:12:57 +0600

    Fix damage proportions for cases when loser is more ppowerful than winner

--- app/models/fight.rb
+++ app/models/fight.rb
@@ -108,12 +108,12 @@ class Fight < ActiveRecord::Base
       victim_damage_reduce    = 0.01 * victim.assignments.effect_value(:fight_damage)
 
       attack_damage   = rand(victim.health * 1000) * 0.5 * (1 - victim_damage_reduce)
-      defence_damage  = rand(attack_damage * defence / attack)
+      defence_damage  = rand(attack_damage * (attack > defence ? defence / attack : 0.9))
     else
       attacker_damage_reduce  = 0.01 * attacker.assignments.effect_value(:fight_damage)
 
       defence_damage  = rand(attacker.health * 1000) * 0.5 * (1 - attacker_damage_reduce)
-      attack_damage   = rand(defence_damage * attack / defence)
+      attack_damage   = rand(defence_damage * (defence > attack ? attack / defence : 0.9))
     end
 
     return [attacker_won, (attack_damage / 1000).ceil, (defence_damage / 1000).ceil]
