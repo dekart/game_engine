2200c5dc05c6819644dbbae2decb266ed241a782
DEkart <dekart@Lamptop.(none)>
2009-08-30 22:38:19 +0600

    Make promotions forwardable

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090830071542) do
+ActiveRecord::Schema.define(:version => 20090830162017) do
 
   create_table "assignments", :force => true do |t|
     t.integer  "relation_id"
@@ -234,6 +234,7 @@ ActiveRecord::Schema.define(:version => 20090830071542) do
     t.integer  "promotion_receipts_count", :default => 0
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "forwardable"
   end
 
   create_table "properties", :force => true do |t|
