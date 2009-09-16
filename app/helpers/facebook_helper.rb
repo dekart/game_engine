02bb259a4c17dc3b5519fb8778795712c0c37bce
DEkart <dekart@Lamptop.(none)>
2009-09-17 00:02:05 +0600

    Remove Play Now button to fit Platform Guidelines

--- app/helpers/facebook_helper.rb
+++ app/helpers/facebook_helper.rb
@@ -89,7 +89,6 @@ module FacebookHelper
     options[:template_data] ||= {}
     options[:template_data][:images] ||= []
     options[:template_data][:images].unshift(:src => image_path("logo_100x100.jpg"), :href => image_url)
-    options[:template_data][:images].push(:src => image_path("play_now_small.jpg"), :href => image_url)
     
     "Facebook.showFeedDialog(#{bundle_id}, #{options[:template_data].to_json}, '#{options[:body_general]}', #{options[:target_id] || "null"}, function(){#{options[:continuation]}})"
   end
