61a0af2dff1ee2817bee12edb176397a00940783
DEkart <dekart@Lamptop.(none)>
2009-09-29 21:21:40 +0600

    Fix deployment task

--- config/deploy.rb
+++ config/deploy.rb
@@ -92,7 +92,11 @@ before "deploy:migrations", "deploy:db:backup"
   before "deploy:migrations", t
 end
 
-["deploy:bootstrap", "deploy:jobs:update_references", "deploy:jobs:start", "deploy:cleanup", "deploy:update_apache_config"].each do |t|
+["deploy:jobs:update_references", "deploy:cleanup"].each do |t|
   after "deploy", t
   after "deploy:migrations", t
+end
+
+["deploy:jobs:start", "deploy:update_apache_config"].each do |t|
+  after "deploy:cold"
 end
