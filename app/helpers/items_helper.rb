650dea7355454a2abc6ce2ddbcdf9b4cb7a09ade
DEkart <dekart@Lamptop.(none)>
2009-10-06 18:59:11 +0600

    Unify item image displayment format

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,11 @@
 module ItemsHelper
+  def item_image(item, format)
+    if item.image?
+      fb_tag(:img, 
+        fb_ta(:src, image_path(item.image.url(format))) + 
+        fb_ta(:alt, fb_i(item.name)) + 
+        fb_ta(:title, fb_i(item.name))
+      )
+    end
+  end
 end
