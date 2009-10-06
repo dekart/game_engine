875178e68178d99c8821bd7fb2bac30056a7c523
DEkart <dekart@Lamptop.(none)>
2009-10-06 16:16:10 +0600

    Move character mission groups extension to separate module

---
+++ app/models/character/mission_groups.rb
@@ -0,0 +1,22 @@
+class Character
+  module MissionGroups
+    def current(new_group_id = nil)
+      if new_group_id and new_group_id.to_i != proxy_owner.current_mission_group_id
+        proxy_owner.update_attribute(:current_mission_group_id, new_group_id)
+      end
+
+      group = MissionGroup.find(proxy_owner.current_mission_group_id) if proxy_owner.current_mission_group_id
+      group ||= MissionGroup.first
+    end
+
+    def completed?(group)
+      if rank = proxy_owner.mission_group_ranks.find_by_mission_group_id(group.id)
+        rank.completed?
+      else
+        group.missions.count(
+          :conditions => ["missions.id NOT IN (?)", proxy_owner.ranks.completed_mission_ids]
+        ) == 0
+      end
+    end
+  end
+end
