8e63a4d83bcbaf5c5b8c2361f4b9dbfd71f16453
DEkart <dekart@Lamptop.(none)>
2009-12-19 13:24:46 +0500

    Basically implement inline tutorial block

--- app/helpers/tutorials_helper.rb
+++ app/helpers/tutorials_helper.rb
@@ -1,2 +1,7 @@
 module TutorialsHelper
+  def tutorial_step(controller, action, completed)
+    current = (controller_name == controller && action_name == action)
+    
+    yield(current, completed)
+  end
 end
