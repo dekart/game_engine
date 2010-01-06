53e0ef86cc92a31320df25330bf3f43a858a08cd
DEkart <dekart@Lamptop.(none)>
2010-01-06 20:55:30 +0500

    Refactor help requests

--- app/helpers/help_requests_helper.rb
+++ app/helpers/help_requests_helper.rb
@@ -4,4 +4,15 @@ module HelpRequestsHelper
       link_to_function(label, help_request_stream_dialog(context), options)
     end
   end
+
+  def latest_help_request_for(context)
+    help_request = current_character.help_requests.latest(:mission)
+
+    if help_request.try(:should_be_displayed?)
+      render("help_requests/latest",
+        :help_request => help_request,
+        :context      => context
+      )
+    end
+  end
 end
