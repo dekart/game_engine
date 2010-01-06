7ce20fdf2d8d0683e01fdb535b6d24e80aa44f3b
DEkart <dekart@Lamptop.(none)>
2010-01-06 18:59:04 +0500

    Oops, forgot the gem

--- app/models/character/boss_fights.rb
+++ app/models/character/boss_fights.rb
@@ -1,7 +1,11 @@
 class Character
   module BossFights
+    def won_boss_ids
+      with_state(:won).all(:select => "DISTINCT boss_id").collect{|f| f.boss_id }
+    end
+
     def find_by_boss(boss)
-      find_by_boss_id_and_workflow_state(boss.id, "progress") || build_by_boss(boss)
+      with_state(:progress).find_by_boss_id(boss.id) || build_by_boss(boss)
     end
 
     def build_by_boss(boss)
