3592db5ae01aa863ad2f701533eca88b61f5f449
DEkart <dekart@Lamptop.(none)>
2010-02-17 01:25:52 +0500

    Destroy invitations on user deletion

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_one :character, :dependent => :destroy
 
-  has_many :invitations, :foreign_key => :sender_id do
+  has_many :invitations, :foreign_key => :sender_id, :dependent => :destroy do
     def facebook_ids
       self.find(:all, :select => "invitations.receiver_id").collect{|i| i[:receiver_id].to_i }
     end
