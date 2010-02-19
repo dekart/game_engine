06dac1c321e3570e8c4b41d85e463d2693fa350c
DEkart <dekart@Lamptop.(none)>
2010-02-19 21:43:09 +0500

    Fix help request publishing, fix styling

--- public/javascripts/application_old.js
+++ public/javascripts/application_old.js
@@ -8,16 +8,3 @@ var Spinner = {
     $('spinner').show();
   }
 }
-
-
-
-var HelpRequest = {
-  create: function(context_id, context_type){
-    new Ajax.Request(root_url + "help_requests", {
-      parameters: "context_id=" + context_id + "&context_type=" + context_type,
-      method: "POST",
-      "scrollToTop": false,
-      "showSpinner": false
-    });
-  }
-};
