6a09cf6ab78b612145386ed2f83f00de21ef655f
DEkart <dekart@Lamptop.(none)>
2010-03-07 19:27:48 +0500

    Implement Sometrics.com offer wall support

--- lib/facebook_money.rb
+++ lib/facebook_money.rb
@@ -18,7 +18,7 @@ module FacebookMoney
       def valid_request?(params)
         digest = Digest::MD5.hexdigest([params[:id], params[:new], params[:uid], FacebookMoney.config["secret"]].join(":"))
 
-        return digest == params[:sig] && !user(params).nil?
+        digest == params[:sig] && !user(params).nil?
       end
 
       def user(params)
@@ -56,7 +56,7 @@ module FacebookMoney
       def valid_request?(params)
         digest = Digest::MD5.hexdigest([params[:uid], params[:amount], params[:time], params[:oid], FacebookMoney.config["secret"]].join(""))
 
-        return digest == params[:sig] && !user(params).nil?
+        digest == params[:sig] && !user(params).nil?
       end
 
       def user(params)
@@ -93,9 +93,7 @@ module FacebookMoney
       def valid_request?(params)
         digest = Digest::MD5.hexdigest("uid=#{params[:uid]}currency=#{params[:currency]}type=#{params[:type]}ref=#{params[:ref]}#{FacebookMoney.config["secret"]}")
 
-        Rails.logger.debug digest
-        
-        return digest == params[:sig] && !user(params).nil?
+        digest == params[:sig] && !user(params).nil?
       end
 
       def user(params)
@@ -123,6 +121,43 @@ module FacebookMoney
 
       def failure_code
         "ERROR"
+      end
+    end
+  end
+
+  class Sometric
+    class << self
+      def valid_request?(params)
+        digest = Digest::MD5.hexdigest("#{params[:uid]}#{params[:txn_value]}#{FacebookMoney.config["secret"]}#{params[:txn_date]}")
+
+        digest == params[:txn_sig] && !user(params).nil?
+      end
+
+      def user(params)
+        User.find_by_facebook_id(params[:uid])
+      end
+
+      def amount(params)
+        params[:txn_value].to_i
+      end
+
+      def html_code(template, options = {})
+        default_options = {
+          :src => "http://v.sometrics.com/vc_delivery.html?zid=#{FacebookMoney.config["zid"]}&uid=#{template.current_user.facebook_id}&pid=#{FacebookMoney.config["pid"]}",
+          :frameborder  => 0,
+          :width        => 600,
+          :height       => 2400
+        }
+
+        template.content_tag("iframe", "", default_options.merge(options))
+      end
+
+      def success_code
+        "1"
+      end
+
+      def failure_code
+        "0"
       end
     end
   end
