ce05288c4ec2e4c9671ec6db1127b66476d753b3
DEkart <dekart@Lamptop.(none)>
2010-03-09 00:21:21 +0500

    Updating div_form_builder plugin

--- vendor/plugins/div_form_builder/lib/div_form_builder.rb
+++ vendor/plugins/div_form_builder/lib/div_form_builder.rb
@@ -1,44 +1,65 @@
 class DivFormBuilder < ActionView::Helpers::FormBuilder
   REQUIRED_MARK = %{<span class="required">*</span>}
-  FIELD_ORDER = [:label, :required, :before_field, :input, :after_field, :error_message, :previous_value, :comment]
-  CUSTOM_OPTIONS = [:label, :required, :comment, :before_field, :after_field]
+  FIELD_ORDER = {
+    :default    => [:label, :required, :before_field, :input, :after_field, :error_message, :previous_value, :comment],
+    :check_box  => [:before_field, :input, :after_field, :label, :required, :error_message, :comment]
+  }
 
-  (field_helpers + %w(date_select datetime_select) - %w(hidden_field)).each do |selector|
+  CUSTOM_OPTIONS = [:label, :required, :comment, :before_field, :after_field, :field_type, :wrapper]
+
+  (field_helpers + %w(date_select datetime_select) - %w(hidden_field fields_for)).each do |selector|
     src = <<-END_SRC
       def #{selector}(field_name, options = {}, &block)
-        field(field_name, super(field_name, options.except(CUSTOM_OPTIONS)), options, &block)
+        options.reverse_merge!(:field_type => :#{selector})
+
+        field(field_name, super(field_name, options.except(*CUSTOM_OPTIONS)), options, &block)
       end
     END_SRC
+
     class_eval src, __FILE__, __LINE__
   end
 
   def select(field_name, choices, options = {}, html_options = {}, &block)
-    field(field_name, super(field_name, choices, options.except(CUSTOM_OPTIONS), html_options), options, &block)
+    options.reverse_merge!(:field_type => :select)
+
+    field(field_name, super(field_name, choices, options.except(*CUSTOM_OPTIONS), html_options), options, &block)
   end
 
   def grouped_select(field_name, choices, options = {}, html_options = {}, &block)
-    field(field_name, @template.grouped_select(@object_name, field_name, choices, objectify_options(options.except(CUSTOM_OPTIONS)), @default_options.merge(html_options)), options, &block)
+    options.reverse_merge!(:field_type => :group_select)
+
+    field(field_name,
+      @template.grouped_select(@object_name, field_name, choices,
+        objectify_options(options.except(*CUSTOM_OPTIONS)),
+        @default_options.merge(html_options)
+      ),
+      options, &block
+    )
   end
 
   def radio_button(field_name, choises, options = {}, &block)
     code = ""
-    
+
     choises.each do |choise|
       code << div(
-        div(super(field_name, choise[1], options.except(CUSTOM_OPTIONS)), :class => :option_input) +
-        div(choise[0], :class => :option_label),
+        div(super(field_name, choise[1], options.except(*CUSTOM_OPTIONS)), :class => :option_input) +
+        div(label(choise[0], "#{field_name}_#{choise[1]}"), :class => :option_label),
         :class => :option
       )
     end
+
+    options.reverse_merge!(:field_type => :radio_button)
 
     field(field_name, code, options, &block)
   end
 
   def submit(*args)
     options = args.extract_options!
-    text = args.any? ? args.first : @template.t(".submit")
+    text = args.any? ? args.first : @template.t(".submit", :default => @template.t("form_builder.submit"))
 
-    field(:submit, @template.submit_tag(text, options.except(CUSTOM_OPTIONS)), options.merge(:label => false))
+    options.reverse_merge!(:field_type => :submit)
+
+    field(:submit, @template.submit_tag(text, options.except(*CUSTOM_OPTIONS)), options.merge(:label => false))
   end
 
   def fields(partial = nil)
@@ -58,16 +79,16 @@ class DivFormBuilder < ActionView::Helpers::FormBuilder
   def field(field_name, field, options = {}, &block)
     code = ""
 
-    field_changes = object.changes[field_name.to_s] if @options[:show_changes]
+    field_changes = object.respond_to?(:changes) ? object.changes[field_name.to_s] : nil
 
-    (options[:order] || FIELD_ORDER).each do |part|
+    detect_field_order(options[:order] || options[:field_type]).each do |part|
       code << (
         case part
         when :label
           div(
             label(
               options[:label].blank? ? object.class.human_attribute_name(field_name.to_s) : options[:label],
-              "#{@object_name}_#{field_name}"
+              field_name
             ),
             :class => :label
           ) unless options[:label] == false
@@ -84,7 +105,7 @@ class DivFormBuilder < ActionView::Helpers::FormBuilder
         when :previous_value
           if @options[:show_changes] && field_changes
             div(
-              @template.content_tag(:span, @template.t("form_builder.previous_value"), :class => :label) + 
+              @template.content_tag(:span, @template.t("form_builder.previous_value"), :class => :label) +
               " " +
               @template.content_tag(:span, field_changes.first, :class => :value),
               :class => :previous_value
@@ -96,17 +117,17 @@ class DivFormBuilder < ActionView::Helpers::FormBuilder
       ).to_s
     end
 
-    field_class = "field"
+    field_classes = ["field", options[:field_type].to_s]
 
     if @options[:show_changes] && field_changes
-      field_class << " changed" unless field_changes.first.blank? && field_changes.last.blank?
+      field_classes << "changed" unless field_changes.first.blank? && field_changes.last.blank?
     end
 
     code = div(code,
-      :class  => field_class,
+      :class  => field_classes.compact.uniq.join(" "),
       :id     => options[:id] || ("field_for_#{@object_name}_#{field_name}" unless field_name.blank?)
-    )
-    
+    ) unless options[:wrapper] == false
+
     block_given? ? @template.concat(code) : code
   end
 
@@ -126,8 +147,13 @@ class DivFormBuilder < ActionView::Helpers::FormBuilder
     @template.content_tag(:div, content, options)
   end
 
-  def label(text, for_field)
-    @template.content_tag(:label, text, :for => for_field)
+  def label(text, field_name)
+    @template.content_tag(:label, text, :for => "#{@object_name}_#{field_name}")
   end
 
+  def detect_field_order(value)
+    return FIELD_ORDER[:default] unless value
+
+    value.is_a?(Array) ? value : (FIELD_ORDER[value.to_sym] || FIELD_ORDER[:default])
+  end
 end
