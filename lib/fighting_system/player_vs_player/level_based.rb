528f40ebe8472e95a063f379ad5165c5248f7ead
DEkart <dekart@Lamptop.(none)>
2010-04-25 13:26:40 +0600

    Cleanup level-based calculation

--- lib/fighting_system/player_vs_player/level_based.rb
+++ lib/fighting_system/player_vs_player/level_based.rb
@@ -1,14 +1,11 @@
 module FightingSystem
   module PlayerVsPlayer
     module LevelBased
-      LEVEL_DIFF = 3
-      CHANCE = 10 # %
-      
       def self.calculate(attacker, victim)
-        if attacker.level - victim.level > LEVEL_DIFF
-          rand(1000) > CHANCE * 10
-        elsif victim.level - attacker.level > LEVEL_DIFF
-          rand(1000) < CHANCE * 10
+        if attacker.level - victim.level > Setting.i(:fight_level_threshold)
+          rand(1000) > Setting.i(:fight_level_theshold_chance) * 10
+        elsif victim.level - attacker.level > Setting.i(:fight_level_threshold)
+          rand(1000) < Setting.i(:fight_level_theshold_chance) * 10
         else
           attack_points   = attacker.attack_points
           defence_points  = victim.defence_points
@@ -22,38 +19,6 @@ module FightingSystem
           (rand((attack + defence).to_i) >= defence)
         end
       end
-
-=begin
-      def self.calculate(attacker, victim)
-        attack_points   = attacker.attack_points
-        defence_points  = victim.defence_points
-        attack_bonus    = 1.0
-        defence_bonus   = 1.0
-
-        attack = attack_points * attack_bonus * 50
-        defence = defence_points * defence_bonus * 50
-
-        attacker_won = (rand((attack + defence).to_i) >= defence)
-
-        if attacker_won
-          victim_damage_reduce = 0.01 * victim.fight_damage_reduce
-
-          attack_damage   = rand(Setting.p(:fight_max_loser_damage, victim.health * 1000)) * (1 - victim_damage_reduce)
-          defence_damage  = rand(
-            attack > defence ? (attack_damage * defence / attack) : Setting.p(:fight_max_winner_damage, attack_damage)
-          )
-        else
-          attacker_damage_reduce = 0.01 * attacker.fight_damage_reduce
-
-          defence_damage  = rand(Setting.p(:fight_max_loser_damage, attacker.health * 1000)) * (1 - attacker_damage_reduce)
-          attack_damage   = rand(
-            defence > attack ? (defence_damage * attack / defence) : Setting.p(:fight_max_winner_damage, defence_damage)
-          )
-        end
-
-        [attacker_won, (attack_damage / 1000).ceil, (defence_damage / 1000).ceil]
-      end
-=end
     end
   end
 end
