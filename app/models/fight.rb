59e65703376c98d4e259a3be6440e37f8a4ed5e0
DEkart <dekart@Lamptop.(none)>
2010-04-25 12:32:29 +0600

    Remove legacy fight calculation method

--- app/models/fight.rb
+++ app/models/fight.rb
@@ -106,36 +106,6 @@ class Fight < ActiveRecord::Base
     self.victim.save!
   end
 
-  def calculate_proportions(attacker, victim)
-    attack_points   = attacker.attack_points
-    defence_points  = victim.defence_points
-    attack_bonus    = 1.0
-    defence_bonus   = 1.0
-
-    attack = attack_points * attack_bonus * 50
-    defence = defence_points * defence_bonus * 50
-
-    attacker_won = (rand((attack + defence).to_i) >= defence)
-
-    if attacker_won
-      victim_damage_reduce    = 0.01 * victim.assignments.effect_value(:fight_damage)
-
-      attack_damage   = Setting.p(:fight_max_loser_damage, rand(victim.health * 1000) * (1 - victim_damage_reduce))
-      defence_damage  = rand(
-        attack > defence ? (attack_damage * defence / attack) : Setting.p(:fight_max_winner_damage, attack_damage)
-      )
-    else
-      attacker_damage_reduce  = 0.01 * attacker.assignments.effect_value(:fight_damage)
-
-      defence_damage  = rand(Setting.p(:fight_max_loser_damage, attacker.health * 1000)) * (1 - attacker_damage_reduce)
-      attack_damage   = rand(
-        defence > attack ? (defence_damage * attack / defence) : Setting.p(:fight_max_winner_damage, defence_damage)
-      )
-    end
-
-    return [attacker_won, (attack_damage / 1000).ceil, (defence_damage / 1000).ceil]
-  end
-
   def update_victim_dashboard
     Delayed::Job.enqueue Jobs::FightNotification.new(self.id)
   end
