0bf9c234298f48f61228ad74c1f91b1954487459
DEkart <dekart@Lamptop.(none)>
2010-05-12 12:09:07 +0600

    Increase statistics limit

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -2,6 +2,6 @@ class Admin::StatisticsController < Admin::BaseController
   def index
     @total_users    = User.count
     @last_day_users = User.count(:conditions => ["created_at >= ?", 24.hours.ago])
-    @latest_users   = User.find(:all, :order => "created_at DESC", :limit => 5)
+    @latest_users   = User.find(:all, :order => "created_at DESC", :limit => 100)
   end
 end
