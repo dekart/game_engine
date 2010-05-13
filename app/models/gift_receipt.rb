082af97f38af2941a91af8d0247cd70814765df8
Eugene Korbut <me@mvl.ru>
2010-05-14 04:17:08 +1100

    Accept all gifts refactor

--- app/models/gift_receipt.rb
+++ app/models/gift_receipt.rb
@@ -2,11 +2,16 @@ class GiftReceipt < ActiveRecord::Base
   belongs_to :gift, :counter_cache => :receipts_count
   belongs_to :character
 
-  after_create :give_item_to_character
+  named_scope :unused, :conditions => { :accepted => false }
 
-  protected
+  named_scope :for_character, lambda { |character|
+    { :conditions => [ "character_id = ? OR facebook_id = ?", character.id, character.user.facebook_id ] }
+  }
 
-  def give_item_to_character
+  def give_item_to_character!
+    self.character = User.find_by_facebook_id(facebook_id).character
+    self.accepted = true
+    save!
     self.character.inventories.give!(self.gift.item)
   end
 end
