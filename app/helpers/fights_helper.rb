075312a9ebf5fa8fb4344781d9bba120c645c24d
DEkart <dekart@Lamptop.(none)>
2010-05-22 19:28:01 +0600

    Use classes to style fight list instead of tags

--- app/helpers/fights_helper.rb
+++ app/helpers/fights_helper.rb
@@ -1,12 +1,14 @@
 module FightsHelper
   def fight_description(fight)
     t("fights.descriptions.#{ fight.attacker == current_character ? "attack" : "defence" }_#{ fight.winner == current_character ? "won" : "lost" }",
+      
       :attacker           => link_to(character_name(fight.attacker), fight.attacker),
       :victim             => link_to(character_name(fight.victim), fight.victim),
-      :money              => content_tag(:span, number_to_currency(fight.money), :class => :basic_money),
-      :experience_points  => content_tag(:span, fight.experience, :class => :experience),
-      :attacker_damage    => content_tag(:span, fight.attacker_hp_loss, :class => :health),
-      :victim_damage      => content_tag(:span, fight.victim_hp_loss, :class => :health)
+
+      :money              => content_tag(:span, number_to_currency(fight.money),  :class => "attribute basic_money"),
+      :experience_points  => content_tag(:span, fight.experience,                 :class => "attribute experience"),
+      :attacker_damage    => content_tag(:span, fight.attacker_hp_loss,           :class => "attribute health"),
+      :victim_damage      => content_tag(:span, fight.victim_hp_loss,             :class => "attribute health")
     )
   end
   safe_helper :fight_description
