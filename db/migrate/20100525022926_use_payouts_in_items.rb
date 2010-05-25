0b58e8631f978e159b78e78733d75cea92dd889d
DEkart <dekart@Lamptop.(none)>
2010-05-25 22:07:54 +0600

    Remove usage limit from items

--- db/migrate/20100525022926_use_payouts_in_items.rb
+++ db/migrate/20100525022926_use_payouts_in_items.rb
@@ -3,8 +3,21 @@ class UsePayoutsInItems < ActiveRecord::Migration
     change_table :items do |t|
       t.text :payouts
     end
+
+    Rake::Task["app:maintenance:use_payouts_for_item_effects"].execute
+
+    change_table :items do |t|
+      t.remove :effects
+      t.remove :usage_limit
+    end
   end
 
   def self.down
+    change_table :items do |t|
+      t.remove :payouts
+
+      t.text    :effects
+      t.integer :usage_limit
+    end
   end
 end
