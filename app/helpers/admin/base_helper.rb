98011a5ec6ba231ee42d133c01feb3a8ae990e5b
DEkart <dekart@Lamptop.(none)>
2010-06-06 14:59:15 +0600

    Adding document titles to admin pages

--- app/helpers/admin/base_helper.rb
+++ app/helpers/admin/base_helper.rb
@@ -2,4 +2,10 @@ module Admin::BaseHelper
   def admin_state(object)
     content_tag(:span, object.state.to_s.capitalize, :class => object.state)
   end
+
+  def admin_title(value)
+    @admin_title = value
+
+    content_tag(:h1, value)
+  end
 end
