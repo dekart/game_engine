612ee34bec51c420c3f68570f5e7007043a5f6a8
DEkart <dekart@Lamptop.(none)>
2010-06-09 18:50:01 +0600

    Improve user statistic: add references, display character types, display links to profile

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -3,5 +3,12 @@ class Admin::StatisticsController < Admin::BaseController
     @total_users    = User.count
     @last_day_users = User.count(:conditions => ["created_at >= ?", 24.hours.ago])
     @latest_users   = User.find(:all, :order => "created_at DESC", :limit => 100)
+
+    @total_reference_stats  = User.reference_stats
+    day_reference_stats     = User.reference_stats(24.hours.ago)
+
+    @total_reference_stats.collect! do |name, count|
+      [name, count, day_reference_stats.assoc(name).try(:last).to_i]
+    end
   end
 end
