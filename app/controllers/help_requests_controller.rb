fca19b1875df9539d99180325a53d32fd1cb9a8c
DEkart <me@banana>
2010-06-25 23:41:45 +0600

    Remove friendship check from help requests

--- app/controllers/help_requests_controller.rb
+++ app/controllers/help_requests_controller.rb
@@ -1,4 +1,6 @@
 class HelpRequestsController < ApplicationController
+  skip_landing_redirect
+  
   def create
     if current_character.help_requests.can_publish?(params[:context_type])
       @context = params[:context_type].classify.constantize.find(params[:context_id])
@@ -12,11 +14,13 @@ class HelpRequestsController < ApplicationController
   # FIXME This should work even if user haven't installed the application yet. Visited url should
   # be stored to session, user signs up and see an option to go back and help his friend
   def show
-    if @character = Character.find_by_id(params[:id])
-      if friend?(@character.user) and @help_request = @character.help_requests.latest(params[:context])
-        @help_result  = @help_request.help_results.create(:character => current_character)
+    @character = Character.find_by_id(params[:id])
 
-        @fight = @help_result.fight if @help_request.context.is_a?(Fight) and !@help_result.new_record?
+    if @character and @help_request = @character.help_requests.latest(params[:context])
+      @help_result  = @help_request.help_results.create(:character => current_character)
+
+      if !@help_result.new_record? and @help_request.context.is_a?(Fight)
+        @fight = @help_result.fight
       end
     else
       redirect_to root_path
