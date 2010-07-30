b92c45c5bc951a4cb7d6e7aee57103307f40caa8
DEkart <me@banana>
2010-07-31 00:07:34 +0600

    Display totals at the statistics dashboard

--- app/models/statistics/dashboard.rb
+++ app/models/statistics/dashboard.rb
@@ -1,15 +1,15 @@
 class Statistics
   class Dashboard < self
-    attr_reader :users
+    attr_reader :users, :vip_money
 
     delegate :total_users, :users_by_period, :to => :users
+    delegate :total_deposit, :total_withdraw, :deposit_by_period, :withdraw_by_period, :to => :vip_money
 
     def initialize(*args)
       super(*args)
 
       @users = Users.new(@period)
+      @vip_money = VipMoney.new(@period)
     end
-
-    
   end
 end
