8745cbe3eb8288a792c3f2dba04e7076af31de6b
DEkart <me@banana>
2010-08-17 18:35:03 +0600

    Allow to filter vip money operations by reference type

--- app/controllers/admin/vip_money_operations_controller.rb
+++ app/controllers/admin/vip_money_operations_controller.rb
@@ -1,8 +1,14 @@
 class Admin::VipMoneyOperationsController < Admin::BaseController
-  helper_method :operation_type
+  helper_method :operation_type, :operation_class
   
   def index
-    @operations = operation_class.latest.paginate(:page => params[:page], :per_page => 100)
+    scope = operation_class
+
+    if params[:reference_type]
+      scope = scope.by_reference_type(params[:reference_type])
+    end
+
+    @operations = scope.latest.paginate(:page => params[:page], :per_page => 100)
   end
 
   protected
--- app/models/vip_money_operation.rb
+++ app/models/vip_money_operation.rb
@@ -2,11 +2,20 @@ class VipMoneyOperation < ActiveRecord::Base
   belongs_to :character
 
   named_scope :latest, :order => "id DESC"
+  named_scope :by_reference_type, Proc.new{|type|
+    {:conditions => ["reference_type = ?", type]}
+  }
 
   attr_accessible :amount, :reference
 
   validates_presence_of :amount
   validates_numericality_of :amount, :greater_than => 0, :only_integer => true
+
+  def self.reference_type_options_for_select
+    all(:select => "DISTINCT reference_type").collect do |r|
+      [r[:reference_type].humanize, r[:reference_type]]
+    end
+  end
 
   def reference=(value)
     case value
