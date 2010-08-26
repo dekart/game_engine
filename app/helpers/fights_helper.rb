7eb10278a8a94de5ac027a6ac796d8fffd25a8f4
DEkart <me@banana>
2010-08-26 18:22:43 +0600

    Use signed links in recent fight description

--- app/helpers/fights_helper.rb
+++ app/helpers/fights_helper.rb
@@ -2,8 +2,8 @@ module FightsHelper
   def fight_description(fight)
     t("fights.descriptions.#{ fight.attacker == current_character ? "attack" : "defence" }_#{ fight.winner == current_character ? "won" : "lost" }",
       
-      :attacker           => link_to(character_name(fight.attacker), fight.attacker),
-      :victim             => link_to(character_name(fight.victim), fight.victim),
+      :attacker           => link_to(character_name(fight.attacker), character_path(fight.attacker.key)),
+      :victim             => link_to(character_name(fight.victim), character_path(fight.victim.key)),
 
       :money              => content_tag(:span, number_to_currency(fight.money),  :class => "attribute basic_money"),
       :experience_points  => content_tag(:span, fight.experience,                 :class => "attribute experience"),
