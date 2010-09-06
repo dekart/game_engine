102363f6cacae7c422e21372558369c7cc57e199
DEkart <me@banana>
2010-09-06 19:30:04 +0600

    Assign titles only once

--- app/models/character/titles.rb
+++ app/models/character/titles.rb
@@ -3,7 +3,7 @@ class Character
     def self.included(base)
       base.class_eval do
         has_many :character_titles
-        has_many :titles, :through => :character_titles
+        has_many :titles, :through => :character_titles, :uniq => true
       end
     end
   end
