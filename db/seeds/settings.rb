3185ce1f64b5a25a9535d1afd6a5c3a1bd9c055b
DEkart <me@banana>
2010-09-12 20:50:36 +0600

    Group settings by subject

--- db/seeds/settings.rb
+++ db/seeds/settings.rb
@@ -10,17 +10,28 @@ Setting[:assignment_mission_energy_multiplier] ||= 1.0
 Setting[:assignment_mission_energy_divider] ||= 4.0
 Setting[:assignment_mission_income_multiplier] ||= 4.0
 Setting[:assignment_mission_income_divider] ||= 2.0
+
 Setting[:bank_deposit_fee] ||= 10
+
 Setting[:character_attack_upgrade] ||= 1
 Setting[:character_defence_upgrade] ||= 1
 Setting[:character_health_upgrade] ||= 5
 Setting[:character_energy_upgrade] ||= 1
-Setting[:character_health_restore_period] ||= 60
-Setting[:character_energy_restore_period] ||= 120
-Setting[:character_income_calculation_period] ||= 60
-Setting[:character_weakness_minimum] ||= 20
 Setting[:character_points_per_upgrade] ||= 5
 Setting[:character_vip_money_per_upgrade] ||= 0
+Setting[:character_stamina_upgrade] ||= 1
+Setting[:character_stamina_upgrade_points] ||= 2
+Setting[:character_attack_upgrade_points] ||= 1
+Setting[:character_defence_upgrade_points] ||= 1
+Setting[:character_health_upgrade_points] ||= 1
+Setting[:character_energy_upgrade_points] ||= 1
+
+Setting[:character_health_restore_period] ||= 60
+Setting[:character_energy_restore_period] ||= 120
+Setting[:character_stamina_restore_period] ||= 180
+Setting[:character_income_calculation_period] ||= 60
+Setting[:character_weakness_minimum] ||= 20
+
 Setting[:premium_money_price] ||= 5
 Setting[:premium_money_amount] ||= 1000
 Setting[:premium_energy_price] ||= 5
@@ -29,6 +40,9 @@ Setting[:premium_points_price] ||= 5
 Setting[:premium_points_amount] ||= 5
 Setting[:premium_mercenary_price] ||= 10
 Setting[:premium_change_name_price] ||= 10
+Setting[:premium_reset_attributes_price] ||= 10
+Setting[:premium_stamina_price] ||= 5
+
 Setting[:fight_victim_show_limit] ||= 10
 Setting[:fight_victim_levels_lower] ||= 0
 Setting[:fight_victim_levels_higher] ||= 5
@@ -40,55 +54,61 @@ Setting[:fight_max_loser_damage] ||= 50
 Setting[:fight_max_winner_damage] ||= 90
 Setting[:fight_latest_show_limit] ||= 10
 Setting[:fight_alliance_attack] ||= true
+Setting[:fight_max_money] ||= 10000
+
 Setting[:rating_show_limit] ||= 20
+
 Setting[:help_request_expire_period] ||= 24
 Setting[:help_request_display_period] ||= 24
 Setting[:help_request_mission_money] ||= 5
 Setting[:help_request_mission_experience] ||= 10
 Setting[:help_request_fight_money] ||= 10
 Setting[:help_request_fight_experience] ||= 20
+
 Setting[:inventory_sell_price] ||= 50
+
 Setting[:item_show_basic] ||= 10
 Setting[:item_show_special] ||= 3
+
 Setting[:property_sell_price] ||= 50
 Setting[:property_upgrade_limit] ||= 2000
+
 Setting[:user_tutorial_enabled] ||= true
 Setting[:user_invite_page_redirect_enabled] ||= true
 Setting[:user_invite_page_first_visit_delay] ||= 1
 Setting[:user_invite_page_recurrent_visit_delay] ||= 48
 Setting[:user_permission_request_delay] ||= 24
 Setting[:user_admins] ||= "682180971"
+
 Setting[:relation_show_limit] ||= 10
+
 Setting[:newsletter_recipients_per_send] ||= 10
 Setting[:newsletter_send_sleep] ||= 60
+
 Setting[:relation_max_alliance_size] ||= 500
-Setting[:fight_max_money] ||= 10000
+
 Setting[:mission_group_show_limit] ||= 4
 Setting[:mission_completion_dialog] ||= true
+
 Setting[:app_google_analytics_id] ||= ""
+
 Setting[:gifting_enabled] ||= true
 Setting[:gifting_item_show_limit] ||= 10
 Setting[:gifting_page_first_visit_delay] ||= 1
 Setting[:gifting_page_recurrent_visit_delay] ||= 24
 Setting[:gifting_repeat_send_delay] ||= 24
+Setting[:gifting_accept_all] ||= false
+
 Setting[:wall_enabled] ||= true
 Setting[:wall_posts_show_limit] ||= 10
+
 Setting[:boss_max_loser_damage] ||= 10
 Setting[:boss_max_winner_damage] ||= 10
-Setting[:character_stamina_restore_period] ||= 180
-Setting[:premium_stamina_price] ||= 5
-Setting[:character_stamina_upgrade] ||= 1
-Setting[:character_stamina_upgrade_points] ||= 2
-Setting[:character_attack_upgrade_points] ||= 1
-Setting[:character_defence_upgrade_points] ||= 1
-Setting[:character_health_upgrade_points] ||= 1
-Setting[:character_energy_upgrade_points] ||= 1
+
 Setting[:character_default_name] ||= ""
 Setting[:character_equipment_slots] ||= 5
-Setting[:character_relations_per_equipment_slot] ||= 3
 Setting[:character_auto_equipment] ||= false
-Setting[:gifting_accept_all] ||= 0
-Setting[:premium_reset_attributes_price] ||= 10
+Setting[:character_relations_per_equipment_slot] ||= 3
 
 Setting[:hit_list_enabled] ||= true
 Setting[:hit_list_minimum_reward] ||= 10_000
