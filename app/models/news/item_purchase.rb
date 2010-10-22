2f7d448fed46cc693ed75a73ab2cb00713461e50
Dmitry Maksimov <dmtmax@gmail.com>
2010-10-22 08:38:55 +0400

    Moved Item.find to model class, added newsfeed styles file

--- app/models/news/item_purchase.rb
+++ app/models/news/item_purchase.rb
@@ -1,4 +1,17 @@
 module News
   class ItemPurchase < Base
+    def item
+      @item || find_item
+    end
+
+    def amount
+      data[:amount]
+    end
+
+    protected
+
+    def find_item
+      @item = Item.find(data[:item_id])
+    end
   end
 end
