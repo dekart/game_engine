10e3101a133b7673b0245291b707dcb1fc8bf82e
DEkart <me@banana>
2010-10-22 22:55:27 +0600

    Update Google Analytics code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -80,10 +80,9 @@ module ApplicationHelper
           _gaq.push(['_trackPageview']);
 
           (function() {
-            var ga = document.createElement('script');
-            ga.src = 'http://www.google-analytics.com/ga.js';
-            ga.setAttribute('async', 'true');
-            document.documentElement.firstChild.appendChild(ga);
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
           })();
         </script>
       }.html_safe
