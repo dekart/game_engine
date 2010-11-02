a2fe0e61f1b937320a7116cd6d0ff3cbfa6db2df
Dmitry Maksimov <dmtmax@gmail.com>
2010-11-02 22:46:19 +0300

    Change interface of boosts purchasing

--- app/controllers/boosts_controller.rb
+++ app/controllers/boosts_controller.rb
@@ -1,5 +1,6 @@
 class BoostsController < ApplicationController
   def index
+    @item_groups = ItemGroup.with_state(:visible).visible_in_shop
     @boosts = Boost.with_state(:visible).paginate(:page => params[:page], :per_page => 10)
   end
 end
