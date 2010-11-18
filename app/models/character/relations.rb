27d7509322f25dc52f3af8054df36f2b31c1b111
DEkart <me@banana>
2010-11-18 12:46:47 +0500

    Limit maximum allowed alliance size; don't allow to buy mercenaries when user reaches alliance limit

--- app/models/character/relations.rb
+++ app/models/character/relations.rb
@@ -1,7 +1,11 @@
 class Character
   module Relations
     def effective_size
-      size + 1
+      maximum_size? ? Setting.i(:relation_max_alliance_size) : size + 1
+    end
+
+    def maximum_size?
+      size + 1 >= Setting.i(:relation_max_alliance_size)
     end
   end
 end
