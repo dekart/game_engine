81ea3b4b0bcf20e2daa4e3e367a0a31317163bbd
DEkart <me@banana>
2010-12-07 12:09:28 +0500

    Don't pass wrong short links to rails, redirect to root instead

--- app/metal/short_link.rb
+++ app/metal/short_link.rb
@@ -2,21 +2,25 @@ class ShortLink
   extend ApplicationController::ReferenceCode
   
   def self.call(env)
-    if match_data = env["PATH_INFO"].match(/^\/cil\/(\d+-[a-z0-9]{10})/) and key = match_data[1]
-      target_url = "%s/invitations/%s?reference_code=%s" % [
-        Facebooker2.canvas_page_url,
-        key,
-        CGI.escape(
-          reference_code(:invite_link, user_id(key))
-        )
-      ]
+    if match_data = env["PATH_INFO"].match(/^\/cil\/(\d+-[a-z0-9]+)/) and key = match_data[1]
+      if user_id = user_id(key)
+        target_url = "%s/invitations/%s?reference_code=%s" % [
+          Facebooker2.canvas_page_url,
+          key,
+          CGI.escape(reference_code(:invite_link, user_id))
+        ]
 
-      [200, {"Content-Type" => "text/html"}, iframe_redirect_code(target_url)]
+        [200, {"Content-Type" => "text/html"}, iframe_redirect_code(target_url)]
+      else
+        [200, {"Content-Type" => "text/html"}, iframe_redirect_code(Facebooker2.canvas_page_url)]
+      end
     else
       [404, {"Content-Type" => "text/html"}, "Not Found"]
     end
-  rescue
+  rescue Exception => e
+    #TODO Possibly this rescue is not necessary. Remove it if there will be no errors.
     Rails.logger.error "Failed to parse short link: '#{env["PATH_INFO"]}'"
+    Rails.logger.error e
 
     [200, {"Content-Type" => "text/html"}, iframe_redirect_code(Facebooker2.canvas_page_url)]
   end
