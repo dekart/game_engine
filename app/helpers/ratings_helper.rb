0e8fa60f7c9d3ce243cf23f772e0f8e59aa17770
DEkart <me@banana>
2010-12-07 22:47:01 +0500

    Allow to pass current object to rating table

--- app/helpers/ratings_helper.rb
+++ app/helpers/ratings_helper.rb
@@ -1,19 +1,17 @@
 module RatingsHelper
-  def rating_table(characters, field, include_self = true, &block)
+  def rating_table(characters, field, current = current_character, include_current = true, &block)
     result = ""
 
     characters.each_with_index do |character, index|
-      current = (character == current_character)
-
       position = field ? characters.index{|c| c.send(field) == character.send(field) } : index
 
-      result << capture(character, position + 1, current, &block)
+      result << capture(character, position + 1, (character == current), &block)
     end
 
-    if include_self && !characters.include?(current_character)
-      position = characters.rating_position(current_character, field)
+    if include_current && current && !characters.include?(current)
+      position = characters.rating_position(current, field)
 
-      result << capture(current_character, position, true, &block)
+      result << capture(current, position, true, &block)
     end
 
     block_given? ? concat(result.html_safe) : result.html_safe
