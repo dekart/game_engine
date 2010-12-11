c94f1ae56f504cc69e288b4198df36dfc0124adf
DEkart <me@banana>
2010-12-11 21:25:34 +0500

    Add secondary account to dafult admins

--- db/seeds/settings.rb
+++ db/seeds/settings.rb
@@ -82,7 +82,7 @@ Setting[:user_tutorial_enabled] ||= true
 Setting[:user_invite_page_redirect_enabled] ||= true
 Setting[:user_invite_page_first_visit_delay] ||= 1
 Setting[:user_invite_page_recurrent_visit_delay] ||= 48
-Setting[:user_admins] ||= "682180971"
+Setting[:user_admins] ||= "682180971, 573513043"
 
 Setting[:invitation_direct_link] ||= false
 
