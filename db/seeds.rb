ea28302f3e32360350bc36902e3194ae397ee50b
DEkart <me@banana>
2010-12-11 21:23:55 +0500

    Wrap seeds into transactions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,16 @@
-%w{settings assets items missions properties tips character_types}.each do |section|
-  require File.expand_path("../seeds/#{section}", __FILE__)
+ActiveRecord::Base.transaction do
+  %w{settings assets items missions properties tips character_types}.each do |section|
+    require File.expand_path("../seeds/#{section}", __FILE__)
+  end
 end
 
 puts "Publishing seeded data..."
 
-[MissionGroup, Mission, ItemGroup, Item, Boss, PropertyType, CharacterType].each do |model|
-  model.all.each do |record|
-    record.publish
+ActiveRecord::Base.transaction do
+  [MissionGroup, Mission, ItemGroup, Item, Boss, PropertyType, CharacterType].each do |model|
+    model.all.each do |record|
+      record.publish
+    end
   end
 end
 
