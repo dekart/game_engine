618f8687919d8b70e54b346d2fb5cb004ff6d7ab
DEkart <rene.dekart@gmail.com>
2011-03-29 16:43:04 +0600

    Don't mention old class in the gift receipt migration

--- db/migrate/20100513155927_add_accepted_and_facebook_id_to_gift_receipts.rb
+++ db/migrate/20100513155927_add_accepted_and_facebook_id_to_gift_receipts.rb
@@ -2,7 +2,8 @@ class AddAcceptedAndFacebookIdToGiftReceipts < ActiveRecord::Migration
   def self.up
     add_column :gift_receipts, :accepted, :boolean, :null => false, :default => false
     add_column :gift_receipts, :facebook_id, :integer, :limit => 6
-    GiftReceipt.update_all 'accepted = 1'
+    
+    ActiveRecord::Base.connection.execute "UPDATE gift_receipts SET accepted = 1"
   end
 
   def self.down
