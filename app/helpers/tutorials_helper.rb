5519a33e4c5a5734ed46c546ce183ccbf7ebf559
Mikhail Khomutetskiy <khomutetskiy@gmail.com>
2011-03-30 16:58:14 +0600

    current step refactor

--- app/helpers/tutorials_helper.rb
+++ app/helpers/tutorials_helper.rb
@@ -11,27 +11,27 @@ module TutorialsHelper
     yield(current, reload_function)
   end
   
-  def t_step(property, step = exract_current_step)
+  def t_step(property, step = current_step)
     t("tutorial.steps.#{step}.#{property}")
   end
   
-  def step_title(step = exract_current_step)
+  def step_title(step = current_step)
     t_step("title", step)
   end
   
-  def final_step?(step = exract_current_step)
+  def final_step?(step = current_step)
     Tutorial::STEPS.last == step
   end
   
-  def step_index(step = exract_current_step)
+  def step_index(step = current_step)
     Tutorial::STEPS.index(step)
   end
   
-  def next_step(step = exract_current_step)
+  def next_step(step = current_step)
     final_step?(step) ? "" : Tutorial::STEPS[step_index(step) + 1] 
   end
   
-  def exract_current_step
+  def current_step
     if (current_step = current_user.tutorial_step).empty?
       current_step = Tutorial::STEPS.first
     end
@@ -39,7 +39,7 @@ module TutorialsHelper
   end
   
   def show_standard_dialog(options = {})
-    current_step = options[:step] || exract_current_step
+    current_step = options[:step] || current_step
     options[:title] ||= escape_javascript(t_step("window_title", current_step))
     options[:text] ||= escape_javascript(t_step("window_text", current_step))
     options[:button_name] ||= escape_javascript(t_step("window_button_name", current_step))
