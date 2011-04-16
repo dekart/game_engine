ab956d0c334078fa65a4369464980e9aecd23721
DEkart <rene.dekart@gmail.com>
2011-04-16 22:12:52 +0600

    Update level stats to reflect average and max values

--- app/models/statistics/levels.rb
+++ app/models/statistics/levels.rb
@@ -24,19 +24,53 @@ class Statistics
           AVG(missions_succeeded) AS missions_succeeded,
           AVG(missions_mastered) AS missions_mastered,
           AVG(missions_completed) AS missions_completed,
-          (SELECT count(relations.id) FROM relations WHERE owner_id = characters.id AND relations.type = 'MercenaryRelation') AS mercenaries,
-          (SELECT count(relations.id) FROM relations WHERE owner_id = characters.id AND relations.type = 'FriendRelation') AS friends
+
+          MAX(total_money) AS basic_money_max,
+          MAX(vip_money) AS vip_money_max,
+          MAX(attack) AS attack_max,
+          MAX(defence) AS defence_max,
+          MAX(health) AS health_max,
+          MAX(energy) AS energy_max,
+          MAX(stamina) AS stamina_max,
+          MAX(fights_won + fights_lost) AS fights_max,
+          MAX(missions_succeeded) AS missions_succeeded_max,
+          MAX(missions_mastered) AS missions_mastered_max,
+          MAX(missions_completed) AS missions_completed_max
         }, 
         :group  => :level
       ).collect do |record|
         OpenStruct.new.tap do |s|
-          %w{level basic_money attack defence vip_money health energy stamina missions_succeeded missions_mastered missions_completed}.each do |attr|
-            s.send("#{attr}=", record.send(attr))
-          end
-          
-          %w{total mercenaries friends fights}.each do |attr|
+          %w{
+            level 
+            total
+            basic_money
+            attack
+            defence
+            vip_money
+            health
+            energy
+            stamina
+            fights
+            missions_succeeded
+            missions_mastered
+            missions_completed
+            basic_money_max
+            attack_max
+            defence_max
+            vip_money_max
+            health_max
+            energy_max
+            stamina_max
+            fights_max
+            missions_succeeded_max
+            missions_mastered_max
+            missions_completed_max
+          }.each do |attr|
             s.send("#{attr}=", record[attr].to_f.round)
           end
+          
+          s.mercenaries = MercenaryRelation.count(:joins => :owner, :conditions => ["characters.level = ?", record.level]).to_f / s.total
+          s.friends = FriendRelation.count(:joins => :owner, :conditions => ["characters.level = ?", record.level]).to_f / s.total
         end
       end
     end
