0c9690e1f29dcf943ae7612e8a648b42a0ea81e5
Mikhail Khomutetskiy <khomutetskiy@gmail.com>
2011-05-06 14:14:36 +0600

    Refactor change_app_requests migration.  - delete item_id or monster_id from AppRequest data  - refactor copy-paste code

--- db/migrate/20110504083736_change_app_requests_data_to_target.rb
+++ db/migrate/20110504083736_change_app_requests_data_to_target.rb
@@ -2,12 +2,21 @@
 class ChangeAppRequestsDataToTarget < ActiveRecord::Migration
   def self.up
     AppRequest::Base.transaction do
-      AppRequest::Base.find_each(:conditions => ["data is not null and target_id is null"]) do |app_request|
-        if app_request.data['monster_id']
-          app_request.target = Monster.find(app_request.data['monster_id'])
-          app_request.save!
-        elsif app_request.data['item_id']
-          app_request.target = Item.find(app_request.data['item_id'])
+      AppRequest::Base.find_each(:conditions => ["data is not null"]) do |app_request|
+        data = app_request.data
+        
+        if (target_id = data['monster_id'] || data['item_id'])
+          
+          target_type, target_id = data.select {|k, v| ['monster_id', 'item_id'].include?(k) }.first
+          
+          target_class = target_type.gsub(/_id/, '').classify.constantize
+          data.delete(target_type)
+          
+          data['target_id'] = target_id
+          data['target_type'] = target_class.name
+          
+          app_request.target = target_class.find(target_id)
+          
           app_request.save!
         end
       end
