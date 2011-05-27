cbc45b68514f012fddcf0ed08b6a994cb1273f44
DEkart <rene.dekart@gmail.com>
2011-05-27 22:52:53 +0600

    Don't fail on help page link generation if there is no current user

--- app/helpers/help_pages_helper.rb
+++ app/helpers/help_pages_helper.rb
@@ -1,6 +1,6 @@
 module HelpPagesHelper
   def help_link(page_alias, text = nil)
-    if HelpPage.visible?(page_alias) or current_user.admin?
+    if HelpPage.visible?(page_alias) or current_user.try(:admin?)
       link_to(text || asset_image_tag("icons_help"), help_page_path(page_alias), :class => :help)
     end
   end
