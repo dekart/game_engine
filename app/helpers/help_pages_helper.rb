ef2de7cb8a3f941afdb42b69674cbb50f201232c
DEkart <rene.dekart@gmail.com>
2011-05-28 15:32:24 +0600

    Add help link icon in CSS, not in the code

--- app/helpers/help_pages_helper.rb
+++ app/helpers/help_pages_helper.rb
@@ -1,7 +1,7 @@
 module HelpPagesHelper
   def help_link(page_alias, text = nil)
     if HelpPage.visible?(page_alias) or current_user.try(:admin?)
-      link_to(text || asset_image_tag("icons_help"), help_page_path(page_alias), :class => :help)
+      link_to(text || '&nbsp;'.html_safe, help_page_path(page_alias), :class => :help)
     end
   end
 end
