70f4002f483bc434c3f148b9c0ee7dac672d73bc
DEkart <rene.dekart@gmail.com>
2011-06-05 01:35:46 +0600

    Remove debugging

--- config/initializers/facebooker2.rb
+++ config/initializers/facebooker2.rb
@@ -1,6 +1,5 @@
 Facebooker2.load_facebooker_yaml
 
 ActionController::Base.asset_host = Proc.new{|source, request|
-  Rails.logger.debug request.protocol
   Facebooker2.callback_url(request.protocol) unless ENV['OFFLINE']
 }
