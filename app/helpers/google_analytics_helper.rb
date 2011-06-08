44a8ce0d47ebdb9531a3c6f5875ba28a4f0a921e
DEkart <rene.dekart@gmail.com>
2011-06-08 15:37:59 +0600

    Get back HTTPS support to google analytics

--- app/helpers/google_analytics_helper.rb
+++ app/helpers/google_analytics_helper.rb
@@ -13,7 +13,7 @@ module GoogleAnalyticsHelper
 
         (function() {
           var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-          ga.src = '#{request.protocol}www.google-analytics.com/ga.js';
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
           var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
         })();
       </script>
