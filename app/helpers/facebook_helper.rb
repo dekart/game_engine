7d5fa85e0626190d4b034ebc4613a13642d38c84
DEkart <rene.dekart@gmail.com>
2011-06-09 13:11:13 +0600

    Use graph URL to fetch character image instead of XFBML

--- app/helpers/facebook_helper.rb
+++ app/helpers/facebook_helper.rb
@@ -33,6 +33,14 @@ module FacebookHelper
   def fb_custom_req_choice(label, url)
     content_tag("fb:req-choice", fb_fa(:label, label), :url => url)
   end
+  
+  def fb_profile_pic(user, options = {})
+    options = options.dup
+    
+    validate_fb_profile_pic_size(options)
+    
+    image_tag('https://graph.facebook.com/%s/picture?type=%s' % [Facebooker2.cast_to_facebook_id(user), options[:size]])
+  end
 
   def fb_profile_url(user)
     "#{request.protocol}www.facebook.com/profile.php?id=#{user.facebook_id}"
