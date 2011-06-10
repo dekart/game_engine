7403d9c12cb4697f2547103115ced323a71dbe16
DEkart <rene.dekart@gmail.com>
2011-06-10 11:42:36 +0600

    Added help link for alliance equipment slot

--- app/helpers/help_pages_helper.rb
+++ app/helpers/help_pages_helper.rb
@@ -1,7 +1,9 @@
 module HelpPagesHelper
   def help_link(page_alias, text = nil)
     if HelpPage.visible?(page_alias) or current_user.try(:admin?)
-      link_to(text || '&nbsp;'.html_safe, help_page_path(page_alias), :class => :help)
+      link = link_to(text || '&nbsp;'.html_safe, help_page_path(page_alias), :class => :help)
+      
+      block_given? ? yield(link) : link
     end
   end
 end
