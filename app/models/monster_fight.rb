75e15c5b6f1fe5bcb719eac33897ef55d51385f2
DEkart <rene.dekart@gmail.com>
2011-06-16 19:53:04 +0600

    Integer all event values

--- app/models/monster_fight.rb
+++ app/models/monster_fight.rb
@@ -135,11 +135,11 @@ class MonsterFight < ActiveRecord::Base
     {
       :reference_id => self.id,
       :reference_type => "Monster",
-      :reference_damage => -self.monster_damage,
-      :health => -self.character_damage,
-      :basic_money => self.money,
-      :stamina => -self.stamina,
-      :experience => self.experience
+      :reference_damage => -self.monster_damage.to_i,
+      :health => -self.character_damage.to_i,
+      :basic_money => self.money.to_i,
+      :stamina => -self.stamina.to_i,
+      :experience => self.experience.to_i
     }
   end
 
