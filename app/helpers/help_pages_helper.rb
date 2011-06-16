e12a3979ad95856098e2463b03e287c7a7191b48
DEkart <rene.dekart@gmail.com>
2011-06-16 18:53:25 +0600

    Added a title to help link

--- app/helpers/help_pages_helper.rb
+++ app/helpers/help_pages_helper.rb
@@ -1,7 +1,14 @@
 module HelpPagesHelper
   def help_link(page_alias, text = nil)
     if HelpPage.visible?(page_alias) or current_user.try(:admin?)
-      link = link_to(text || '&nbsp;'.html_safe, help_page_path(page_alias), :class => :help)
+      if text
+        link = link_to(text, help_page_path(page_alias), :class => :help)
+      else
+        link = link_to('&nbsp;'.html_safe, help_page_path(page_alias), 
+          :class => :help, 
+          :title => t('help_pages.link.title')
+        )
+      end
       
       block_given? ? yield(link) : link
     end
