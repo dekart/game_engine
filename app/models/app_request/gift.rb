d5d5f6cfcaafe2925dcc4da3da03888d2b8a63a1
DEkart <rene.dekart@gmail.com>
2011-06-24 14:17:15 +0600

    Correctly set expiration key

--- app/models/app_request/gift.rb
+++ app/models/app_request/gift.rb
@@ -13,7 +13,7 @@ class AppRequest::Gift < AppRequest::Base
     end
     
     def accepted_recently?(sender, receiver)
-      ids = Rails.cache.fetch(receiver_cache_key(receiver), :expires_in => 15.minutes) do
+      ids = Rails.cache.fetch(receiver_cache_key(receiver), :expire_in => 15.minutes) do
         with_state(:accepted).
         scoped(:conditions => ["accepted_at >= ?", Setting.i(:gifting_repeat_accept_delay).hours.ago]).
         all(:select => "sender_id").map{|r| r.sender_id }
