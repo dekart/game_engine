eec2ceafe88e473b0396c210d9756d9180f3e453
DEkart <rene.dekart@gmail.com>
2011-08-25 14:48:45 +0600

    Reove debugging

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -1,7 +1,7 @@
 class ContestsController < ApplicationController
   def show  
     @contest = Contest.find(params[:id])
-    logger.debug 1
+
     @current_group = @contest.group_for(current_character)
   end
 end
