9f404d2e617cf61bc6b738e8e869552cdb78bad6
DEkart <rene.dekart@gmail.com>
2011-08-29 22:33:07 +0600

    Remove unnecessary debugging

--- app/metal/character_status.rb
+++ app/metal/character_status.rb
@@ -20,8 +20,6 @@ class CharacterStatus
         user_id = fb_decode_signed_request(request.env['HTTP_SIGNED_REQUEST'])[:user_id]
       end
       
-      Rails.logger.debug(user_id)
-
       if user_id and character = User.find_by_facebook_id(user_id).try(:character)
         [200, {"Content-Type" => "application/json"}, character.to_json_for_overview]
       else
