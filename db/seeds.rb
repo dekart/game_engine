25b7b6146f21aa475757b96f6b7e2a6752202049
DEkart <rene.dekart@gmail.com>
2011-09-06 17:34:18 +0600

    Partially fixed tutorial, updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 ActiveRecord::Base.transaction do
-  %w{settings assets items missions properties tips character_types}.each do |section|
+  %w{settings assets items missions monsters properties tips character_types}.each do |section|
     require File.expand_path("../seeds/#{section}", __FILE__)
   end
 end
@@ -7,7 +7,7 @@ end
 puts "Publishing seeded data..."
 
 ActiveRecord::Base.transaction do
-  [MissionGroup, Mission, ItemGroup, Item, Boss, PropertyType, CharacterType, Tip].each do |model|
+  [MissionGroup, Mission, MonsterType, ItemGroup, Item, Boss, PropertyType, CharacterType, Tip].each do |model|
     model.all.each do |record|
       record.publish
     end
