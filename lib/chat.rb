2da5582bbb7088e5c19087753a853871b921d04e
DEkart <rene.dekart@gmail.com>
2011-09-07 18:11:44 +0600

    Chat refactoring

--- lib/chat.rb
+++ lib/chat.rb
@@ -1,38 +1,38 @@
 class Chat
   class << self
     def messages(chat_id, from_id = nil)
-      messages = $redis.lrange(self.key(chat_id), 0, Setting.i(:chat_max_messages))
+      messages = $redis.lrange(key(chat_id), 0, Setting.i(:chat_max_messages))
       
-      unless from_id.blank?
-        index = messages.index {|m| ActiveSupport::JSON.decode(m)['id'] == from_id}
-        messages = messages[index + 1..messages.length] if index
+      if !from_id.blank? and index = messages.index {|m| ActiveSupport::JSON.decode(m)['id'] == from_id }
+        messages = messages[index + 1 .. -1]
       end
       
       messages
     end
     
     def save_message(chat_id, character_id, text)
-      unless chat_id.blank? || text.blank?
-        character = Character.find(character_id, :joins => :user)
+      return if chat_id.blank? || text.blank?
+      
+      character = Character.find(character_id, :joins => :user)
+      
+      message = {
+        :id             => Digest::MD5.hexdigest("%s-%s" % [Time.now, character.id]),
         
-        text = text[0, Setting.i(:chat_max_length)]
+        :facebook_id    => character.facebook_id,
+        :name           => (character.name.present? ? character.name : character.user.first_name),
+        :character_key  => character.key,
         
-        message = {
-          :id =>  Digest::MD5.hexdigest("%s-%s" % [Time.now, character.id]),
-          :facebook_id => character.user.facebook_id,
-          :text => text,
-          :created_at => Time.now.strftime("%m.%d.%y %H:%M:%S"),
-          :name => character.name,
-          :character_key => character.key
-        }
+        :text           => text.mb_chars[0, Setting.i(:chat_max_length)].to_s,
         
-        $redis.multi do
-          $redis.rpush(self.key(chat_id), message.to_json)
-          $redis.ltrim(self.key(chat_id), -Setting.i(:chat_max_messages), -1)
-        end
-        
-        message
+        :created_at     => Time.now.to_s(:short)
+      }
+      
+      $redis.multi do
+        $redis.rpush(key(chat_id), message.to_json)
+        $redis.ltrim(key(chat_id), - Setting.i(:chat_max_messages), -1)
       end
+      
+      message
     end
     
     def key(chat_id)
