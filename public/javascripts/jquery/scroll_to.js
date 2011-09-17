f699505137903956681a0fec0cc5afb9f3b01a66
DEkart <rene.dekart@gmail.com>
2011-09-17 18:05:10 +0600

    Remove old code

--- public/javascripts/jquery/scroll_to.js
+++ public/javascripts/jquery/scroll_to.js
@@ -3,44 +3,6 @@
     var $target = $(target).eq(0);
     var $offset = $target.offset();
     
-    $offset && window.FB && FB.Canvas.scrollTo(0, $offset.top)
-    /*
-    var styles = {
-      width: "1px",
-      height: "1px",
-      border: "0px",
-      backgroundColor: "transparent",
-      "float": "left",
-      position: 'absolute',
-      left: $offset.left
-    };
-    
-    var $top = $('#_scroller_top');
-    var $bottom = $('#_scroller_bottom');
-    
-    if ($top.length == 0) {
-      var $top = $('<input type="text" id="_scroller_top" />').css(styles).appendTo('body');
-    }
-    
-    if ($bottom.length == 0) {
-      var $bottom = $('<input type="text" id="_scroller_bottom" />').css(styles).appendTo('body');
-    }
-    
-    $bottom.
-      css({ visibility: 'visible', top: $offset.top + 500 }).
-      delay(100).
-      focus().
-      delay(100).
-      css('visibility', 'hidden').
-      blur();
-
-    $top.
-      css({ visibility: 'visible', top: $offset.top - 70 }).
-      delay(100).
-      focus().
-      delay(100).
-      css('visibility', 'hidden').
-      blur();
-    */
+    $offset && window.FB && FB.Canvas.scrollTo(0, $offset.top);
   }
 })(jQuery);
