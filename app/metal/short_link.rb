a7998b7fa87d35e5c1e0e718902d4e6f7c36d765
DEkart <rene.dekart@gmail.com>
2011-09-27 00:04:06 +0600

    Set OpenGraph tags for short links

--- app/metal/short_link.rb
+++ app/metal/short_link.rb
@@ -1,6 +1,8 @@
 class ShortLink
   extend ApplicationController::ReferenceCode
   extend Facepalm::Rails::Controller::Redirects
+  extend ActionView::Helpers::AssetTagHelper 
+  extend AssetsHelper
   
   class << self
     # Required for reference code parsing
@@ -17,8 +19,19 @@ class ShortLink
         if user_id = Character.find_by_invitation_key(key).try(:user_id)
           target_url << "/relations/%s?reference_code=%s" % [key, Rack::Utils.escape(reference_code(:invite_link, user_id))]
         end
+        
+        custom_headers = %{
+          <meta property="og:site_name" content="#{ I18n.t("app_name") }"/>
+          <meta property="og:type" content="game"/>
+          <meta property="og:image" content="#{ asset_image_path(:logo_stream) }"/>
+          <meta property="og:title" content="#{ I18n.t("relations.short_link.title", :app => I18n.t("app_name")) }"/>
+          <meta property="og:description" content="#{ I18n.t("relations.short_link.description", :app => I18n.t("app_name")) }"/>
+
+          <meta property="fb:app_id" content="#{ facepalm.app_id }" />
+          <meta property="fb:admins" content="#{ Setting.a(:user_admins).join(',') }" />
+        }
       
-        [200, {"Content-Type" => "text/html"}, iframe_redirect_code(target_url)]
+        [200, {"Content-Type" => "text/html"}, iframe_redirect_code(target_url, custom_headers)]
       else
         [404, {"Content-Type" => "text/html"}, "Not Found"]
       end
