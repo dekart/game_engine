ebf73078ee421f1ec6dff3c2370fe3f6af83e4fd
DEkart <rene.dekart@gmail.com>
2011-09-26 23:42:15 +0600

    Make current facebook user accessible from views

--- vendor/plugins/facepalm/lib/facepalm/rails/controller.rb
+++ vendor/plugins/facepalm/lib/facepalm/rails/controller.rb
@@ -9,7 +9,7 @@ module Facepalm
           
           before_filter :normal_cookies_for_ie_in_iframes!
           
-          helper_method(:facepalm, :fb_signed_request)
+          helper_method(:facepalm, :fb_signed_request, :current_facebook_user)
         end
       end
       
