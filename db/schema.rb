b1e139190ef9445604291f2f19929f536333a49b
deewild <chizhov.dee@gmail.com>
2011-11-17 19:12:06 +0600

    New estate task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 # It's strongly recommended to check this file into your version control system.
 
 
-ActiveRecord::Schema.define(:version => 20111116060707) do
+ActiveRecord::Schema.define(:version => 20111118062703) do
   create_table "achievement_types", :force => true do |t|
     t.string   "name",               :limit => 250,  :default => "", :null => false
     t.string   "description",        :limit => 1024, :default => "", :null => false
