780c861c069131e1ba6d8100f6389612d05657ee
deewild <chizhov.dee@gmail.com>
2011-11-22 15:41:06 +0600

    achievements sort of collected reward

--- app/helpers/achievements_helper.rb
+++ app/helpers/achievements_helper.rb
@@ -2,7 +2,19 @@ module AchievementsHelper
   def achievement_list(types)
     achievements = current_character.achievements.all
     
-    types.each do |type|
+    types_uncollected = [].tap do |result|
+      achievements.each do |a|
+        unless a.collected?
+          types.reject! do |type|
+            result << type if a.achievement_type_id == type.id
+          end
+        end
+      end
+    end
+
+    types_all = types_uncollected + types
+   
+    types_all.each do |type|
       yield(type, achievements.detect{|a| a.achievement_type_id == type.id })
     end
   end
