57619080552e46325b7c75578828b2d788d172b4
olga <ahmadeeva.ou@gmail.com>
2011-11-22 13:30:20 +0600

    Formatting

--- app/helpers/promo_helper.rb
+++ app/helpers/promo_helper.rb
@@ -31,12 +31,18 @@ module PromoHelper
     
     def html_for_pages
       result = ""
-      pages_to_show = @pages.select { |id, block, options| (!options.has_key?(:context) || options.has_key?(:context) && Array.wrap(options[:context]).include?(context)) }
+
+      pages_to_show = @pages.select { |id, block, options|
+          (!options.has_key?(:context) ||
+          options.has_key?(:context) &&
+          Array.wrap(options[:context]).include?(context))
+      }
 
       if pages_to_show.size > 1
         result << content_tag(:div, "", :class => 'previous')
         result << content_tag(:div, "", :class => 'next')
       end
+
       pages_to_show.each do |id, block, options|
         result << content_tag(:div, capture(&block), :id => "promo_block_page_#{id}", :class => 'page clearfix')
       end
