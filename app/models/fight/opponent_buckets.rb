9d2efafdc82ef5f0fa8396bf9ff885d5930af965
DEkart <rene.dekart@gmail.com>
2011-12-03 22:51:51 +0500

    Replace current opponent buckets with new when new buckets are already rebuilt

--- app/models/fight/opponent_buckets.rb
+++ app/models/fight/opponent_buckets.rb
@@ -4,49 +4,52 @@ class Fight
 
     class << self
       def rebuild!
-        clear!
-      
+        new_bucket_keys = []
+        
         Fight::OPPONENT_LEVEL_RANGES.reverse.each do |range| # Process in reversed order to rebuild small top-level buckets first
-          rebuild_by_range(range)
-        end
-        
-        buckets
-      end
-    
-      def rebuild_by_range(range)
-        ids = Character.connection.select_values(
-          Character.send(:sanitize_sql_array,
-            [
-              "SELECT id FROM characters WHERE level BETWEEN ? AND ? AND fighting_available_at < ?",
-              range.begin,
-              range.end,
-              Time.now.utc
-            ]
+          # Building a list of IDs of characters
+          ids = Character.connection.select_values(
+            Character.send(:sanitize_sql_array,
+              [
+                "SELECT id FROM characters WHERE level BETWEEN ? AND ? AND fighting_available_at < ?",
+                range.begin,
+                range.end,
+                Time.now.utc
+              ]
+            )
           )
-        )
-        ids.map!{|i| i.to_i }
-        
-        ids -= Character.banned_ids
-        ids.shuffle!
-      
-        # Calculating number of opponents per bucket
-        if (ids.size.to_f / BUCKET_SIZE).round > 1 # There are more than bucket_size*1.5 opponents
-          opponents_per_bucket = (ids.size.to_f / (ids.size.to_f / BUCKET_SIZE).ceil).ceil
-        else
-          opponents_per_bucket = ids.size + 1 # Bucket size cannot be zero
-        end
-      
-        buckets = (ids.size.to_f / opponents_per_bucket).ceil # calculating number of buckets for current range
-      
-        # Storing ids in redis splitted by buckets
-        buckets.times do |bucket|
-          ids.slice!(0, opponents_per_bucket).each do |id|
-            $redis.sadd(bucket_key(range.begin, bucket), id) # TODO: Use batch add after migration to new redis gem
+          ids.map!{|i| i.to_i }
+
+          ids -= Character.banned_ids
+          ids.shuffle!
+
+          # Calculating number of opponents per bucket
+          if (ids.size.to_f / BUCKET_SIZE).round > 1 # There are more than bucket_size*1.5 opponents
+            opponents_per_bucket = (ids.size.to_f / (ids.size.to_f / BUCKET_SIZE).ceil).ceil
+          else
+            opponents_per_bucket = ids.size + 1 # Bucket size cannot be zero
           end
+
+          buckets = (ids.size.to_f / opponents_per_bucket).ceil # calculating number of buckets for current range
+
+          # Storing ids in redis splitted by buckets
+          buckets.times do |bucket|
+            new_bucket_keys << bucket_key(range.begin, bucket)
+            
+            ids.slice!(0, opponents_per_bucket).each do |id|
+              $redis.sadd('new_' + new_bucket_keys.last, id) # TODO: Use batch add after migration to new redis gem
+            end
+          end
+
+          # Storing number of buckets for current range
+          $redis.hset("new_opponent_buckets", range.begin, buckets)
         end
-      
-        # Storing number of buckets for current range
-        $redis.hset("opponent_buckets", range.begin, buckets)
+        
+        (new_bucket_keys + ['opponent_buckets']).each do |key|
+          $redis.rename('new_' + key, key)
+        end
+        
+        true
       end
       
       def opponent_ids(range, bucket)
