e25e7d84dc53ba02bd3804f02dcbf68e03ded1f0
Mikhail Khomutetskiy <khomutetskiy@gmail.com>
2012-01-10 15:42:06 +0600

    Init tooltips once

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -807,6 +807,8 @@ $(function(){
       var $element = $(this);
       
       $element.qtip($element.data('tooltip'));
+      
+      $element.removeAttr('data-tooltip');
     });
     
      // Display tooltip on click
@@ -846,6 +848,8 @@ $(function(){
       $element.removeData('qtip');
       
       $element.qtip(tooltipOptions);
+      
+      $element.removeAttr('data-tooltip-on-click');
     });
   })
   
