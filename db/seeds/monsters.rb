eae5a0d6f0b600dbe81509284d94e4812eaa52f3
DEkart <rene.dekart@gmail.com>
2012-01-13 18:43:31 +0500

    Added multi-player monster and boost to seed data

--- db/seeds/monsters.rb
+++ db/seeds/monsters.rb
@@ -25,3 +25,26 @@ MonsterType.create!(
     :image => File.open(Rails.root.join("db", "pictures", "barbarian.jpg"))
   }]
 )
+
+MonsterType.create!(
+  :name => "Hydra (multi-player fight)",
+  :level => 3,
+  :health => 100,
+  :attack => 1,
+  :defence => 1,
+  :minimum_damage => 1,
+  :maximum_damage => 5,
+  :minimum_response => 1,
+  :maximum_response => 5,
+  :fight_time => 12,
+  :experience => 1,
+  :money => 5,
+  :payouts => Payouts::Collection.new(
+    Payouts::VipMoney.new(:value => 1, :apply_on => :victory),
+    Payouts::BasicMoney.new(:value => 200, :apply_on => [:victory, :repeat_victory])
+  ),
+
+  :picture_attributes     => [{
+    :image => File.open(Rails.root.join("db", "pictures", "hydra.jpg"))
+  }]
+)
