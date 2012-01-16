6e972937cfcb21e42413adac317db18ed65ce83e
DEkart <rene.dekart@gmail.com>
2012-01-16 15:45:06 +0500

    Added migration to update damage values for recent monsters

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120105180554) do
+ActiveRecord::Schema.define(:version => 20120116103224) do
 
   create_table "achievement_types", :force => true do |t|
     t.string   "name",               :limit => 250,  :default => "", :null => false
