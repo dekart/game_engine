be70fccaa72f237f0b225445719c8c55e45d6ad9
deewild <chizhov.dee@gmail.com>
2012-01-23 17:32:11 +0600

    Changed causes keys in locales

--- app/helpers/complaints_helper.rb
+++ app/helpers/complaints_helper.rb
@@ -1,9 +1,9 @@
 module ComplaintsHelper
   def causes_list
     [
-      t(".causes.first"),
-      t(".causes.second"), 
-      t(".causes.third")
+      t(".causes.cause_1"),
+      t(".causes.cause_2"), 
+      t(".causes.cause_3")
     ]
   end
 end
