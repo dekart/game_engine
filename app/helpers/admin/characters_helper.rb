c727f0bcb37595ada72827bcea2d434d479f5218
DEkart <rene.dekart@gmail.com>
2012-01-30 20:57:27 +0500

    Added payment stats to character view page

--- app/helpers/admin/characters_helper.rb
+++ app/helpers/admin/characters_helper.rb
@@ -1,2 +1,27 @@
 module Admin::CharactersHelper
+  class PaymentStatsPresenter
+    def initialize(character)
+      @character = character
+    end
+
+    def purchases
+      @character.vip_money_deposits.purchases
+    end
+
+    def total_amount
+      @total_amount ||= purchases.sum(:amount)
+    end
+
+    def total_transactions
+      @total_transactions ||= purchases.count
+    end
+
+    def last_payment_at
+      @last_payment_at ||= purchases.first(:order => "created_at DESC").try(:created_at)
+    end
+  end
+
+  def admin_character_payment_stats(character)
+    yield PaymentStatsPresenter.new(character)
+  end
 end
