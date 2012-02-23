f3ae5e6816acb768f6b740121bebc6057286b866
DEkart <rene.dekart@gmail.com>
2012-02-23 16:20:54 +0500

    Fix Earn Credits button

--- public/javascripts/facebook_credits.js
+++ public/javascripts/facebook_credits.js
@@ -2,15 +2,15 @@
   $.fn.creditPackageForm = function(){
     var $form = $(this);
     var $packages = $form.find('.credit_package');
-    
+
     $packages.click(function(){
       $packages.removeClass('selected');
-      
+
       $(this).addClass('selected').find(':radio').attr('checked', 'true');
     });
-    
+
     $form.find(':radio:checked').parents('.credit_package').addClass('selected');
-    
+
     $form.find('.purchase.button').click(function(){
       FB.ui(
         {
@@ -23,11 +23,15 @@
         }
       );
     });
-    
-    $form.find('.earn_credits.button').click(function(){
+  };
+
+  $.fn.earnCreditButton = function(){
+    var button = $(this);
+
+    button.click(function(){
       FB.ui(
         {
-          method: 'pay', 
+          method: 'pay',
           credits_purchase: true,
           dev_purchase_params: {"shortcut":"offer"}
         },
@@ -36,5 +40,5 @@
         }
       );
     });
-  };
+  }
 })(jQuery);
