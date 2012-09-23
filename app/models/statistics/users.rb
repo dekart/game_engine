685e9327fd1fa7872f58b00b96966952141ee1a4
DEkart <rene.dekart@gmail.com>
2012-09-23 20:45:19 +0600

    Fix user by day stats

--- app/models/statistics/users.rb
+++ app/models/statistics/users.rb
@@ -3,21 +3,24 @@ class Statistics
     def default_scope
       @time_range ? User.where(["users.created_at BETWEEN ? AND ?", @time_range.begin, @time_range.end]) : User
     end
-        
+
     def total_users
       scope.count
     end
-    
+
     def recent_visitors(time = 7.days)
       scope.where(["last_visit_at > ?", time.ago])
     end
-    
+
     def users_by_day
       [].tap do |result|
         numeric_time_range.step(1.day) do |seconds|
           time = Time.at(seconds)
-          
-          result << [time.to_date, scope.count(:conditions => ['users.created_at BETWEEN ? AND ?', time, time.end_of_day])]
+
+          result << [
+            time.to_date,
+            scope.where('users.created_at BETWEEN ? AND ?', time.beginning_of_day, time.end_of_day).count
+          ]
         end
       end
     end
