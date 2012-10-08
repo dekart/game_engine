edd37e61348ab322332e4457bc34de5d6c4a4390
DEkart <rene.dekart@gmail.com>
2012-10-08 19:14:34 +0600

    Save scheduler stats & display them in admin area

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -2,9 +2,20 @@ class Admin::StatisticsController < Admin::BaseController
   def index
     @all = Statistics::Dashboard.new
     @day = Statistics::Dashboard.new(24.hours.ago)
-    
+
     @complaints = Complaint.count
     @unread_complaints = Complaint.with_state(:unread).count
+
+    @delayed_jobs = {
+      :total => Delayed::Job.count,
+      :failed => Delayed::Job.where("last_error IS NOT NULL").count
+    }
+
+    @scheduler = $redis.hgetall(:scheduler).to_options.tap do |s|
+      s[:total_tasks] = s[:total_tasks].to_i
+      s[:started_at] = Time.at(s[:started_at].to_i) unless s[:started_at].blank?
+      s[:finished_at] = Time.at(s[:finished_at].to_i) unless s[:finished_at].blank?
+    end
   end
 
   def user
@@ -12,10 +23,10 @@ class Admin::StatisticsController < Admin::BaseController
     @day = Statistics::Users.new(24.hours.ago)
     @month = Statistics::Users.new(30.days.ago.beginning_of_day)
   end
-  
+
   def level
     @filter = Statistics::Levels::FILTERS.include?(params[:filter]) ? params[:filter] : 'all'
-    
+
     @stats = Statistics::Levels.new(Statistics::Levels.time_frame_by_filter(@filter))
   end
 
@@ -23,20 +34,20 @@ class Admin::StatisticsController < Admin::BaseController
     @all = Statistics::VipMoney.new
     @day = Statistics::VipMoney.new(24.hours.ago)
   end
-  
+
   def visits
     @day  = params[:date] ? Date.parse(params[:date]) : Date.today
     @hour = params[:hour]
-    
+
     result = @hour ? Statistics::Visits.visited_hourly_by_users(@day , @hour) : Statistics::Visits.visited_by_users(@day)
-    
+
     ids = result.collect{|a| a[0]}
     @requests = result.collect{|a| a[1]}
-    
+
     @total = @requests.sum
-   
+
     @users = User.all(:conditions => {:id => ids}).sort_by{ |u| ids.index(u.id) }
-    
+
     @users.insert(ids.index(0), nil) if ids.index(0) #Insert 'no user' value if present
   end
 end
