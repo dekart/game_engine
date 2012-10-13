50fbf4cca8fdbfea31c545c0741b221ab6b89206
DEkart <rene.dekart@gmail.com>
2012-10-13 20:52:43 +0600

    Remove GS

--- lib/gs/backends/redis.rb
+++
@@ -1,9 +0,0 @@
-module GS
-  module Backends
-    module Redis
-      def persist_events(events)
-        ::Redis.current.rpush('gamestats', *events.map{|e| Marshal.dump(e) })
-      end
-    end
-  end
-end
