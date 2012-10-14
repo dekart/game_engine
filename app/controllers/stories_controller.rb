88060bc6d46a69cd212afafd919d798e56f818c8
DEkart <rene.dekart@gmail.com>
2012-10-14 19:22:07 +0600

    Redirect to canvas app from stories if we're in the canvas now

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -20,34 +20,34 @@ class StoriesController < ApplicationController
   def next_page_for_story(key, story_data)
     case key
     when 'item_purchased'
-      items_url(:canvas => true)
+      items_url(:canvas => fb_canvas?)
     when 'mission_help'
       help_mission_url(story_data[:mission_id],
         :key => encryptor.encrypt(
           :mission_id   => story_data[:mission_id],
           :character_id => story_data[:character_id]
         ),
-        :canvas => true
+        :canvas => fb_canvas?
       )
     when 'mission_completed'
-      mission_groups_url(:canvas => true)
+      mission_groups_url(:canvas => fb_canvas?)
     when 'monster_invite'
       monster_url(story_data[:monster_id],
         :key => encryptor.encrypt(story_data[:monster_id]),
-        :canvas => true
+        :canvas => fb_canvas?
       )
     when 'monster_defeated'
-      monsters_url(:canvas => true)
+      monsters_url(:canvas => fb_canvas?)
     when 'property'
-      properties_url(:canvas => true)
+      properties_url(:canvas => fb_canvas?)
     when 'promotion'
       promotion = Promotion.find(story_data[:promotion_id])
 
-      promotion_url(promotion, :canvas => true)
+      promotion_url(promotion, :canvas => fb_canvas?)
     when 'hit_listing_new', 'hit_listing_completed'
-      hit_listings_url(:canvas => true)
+      hit_listings_url(:canvas => fb_canvas?)
     when 'collection_completed'
-      item_collections_url(:canvas => true)
+      item_collections_url(:canvas => fb_canvas?)
     when 'collection_missing_items'
       give_inventories_url(
         :request_data => encryptor.encrypt(
@@ -55,18 +55,18 @@ class StoriesController < ApplicationController
           :valid_till   => story_data[:valid_till],
           :character_id => story_data[:character_id]
         ),
-        :canvas => true
+        :canvas => fb_canvas?
       )
     when 'exchange'
       exchange = Exchange.find(story_data[:exchange_id])
 
-      exchange_url(exchange.key, :canvas => true)
+      exchange_url(exchange.key, :canvas => fb_canvas?)
     when 'achievement'
-      achievements_url(:canvas => true)
+      achievements_url(:canvas => fb_canvas?)
     when 'position_in_rating'
-      rating_url(:canvas => true)
+      rating_url(:canvas => fb_canvas?)
     else
-      root_url(:canvas => true)
+      root_url(:canvas => fb_canvas?)
     end
   end
 end
