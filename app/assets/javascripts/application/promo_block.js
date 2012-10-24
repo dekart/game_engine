89e4b254a625e02d2ccb63fa2a82b8e0cef4e8cd
DEkart <rene.dekart@gmail.com>
2012-10-25 01:01:24 +0600

    Always display contest first in the promo block

--- app/assets/javascripts/application/promo_block.js
+++ app/assets/javascripts/application/promo_block.js
@@ -1,47 +1,47 @@
 (function($){
   $.fn.promoBlock = function(options){
     var element = $(this);
-    
+
     // Checking block existance
     var block = element.data('promoBlock');
-    
+
     if(block){
       return block;
     }
-    
+
     var block = {
       element: element,
       options: $.extend(
         {
           delay : 10 // seconds
-        }, 
+        },
         options
       ),
       paused: false,
-      
+
       setup: function(){
         var max_height = block.pages().map(function(){
           $(this).find('img').load(function(event) {
             var new_height = $(this).parents('.page').outerHeight();
-            
+
             if (new_height > max_height) {
               max_height = new_height;
               $(document).trigger('promo_block.resize');
             }
           });
-          
+
           return $(this).outerHeight();
         }).toArray().sort(function(i,j){ return i > j ? -1 : 1; })[0];
-        
+
         $(document).bind('promo_block.resize', function(){
           block.element.height(max_height);
           block.element.find(".previous, .next").height(max_height);
         });
-        
+
         $(document).trigger('promo_block.resize');
-        
+
         block.element.hover(block.pauseRotation, block.resumeRotation)
-        
+
         block.rotate();
 
         Visibility.every(block.options.delay * 1000, function(){
@@ -60,17 +60,17 @@
 
         block.element.data({promoBlock: block});
       },
-      
+
       rotate: function(){
         var pages = block.pages();
-        
+
         var visible_page = pages.filter('.current');
         var next_page = visible_page.next('.page');
 
         if(next_page.length == 0){
           var next_page = pages.first();
         }
-        
+
         if(next_page[0] != visible_page[0]){
           if(visible_page.length == 0){
             next_page.addClass('current').show();
@@ -81,26 +81,26 @@
           }
         }
       },
-      
+
       pauseRotation: function(){
         block.paused = true;
       },
-      
+
       resumeRotation: function(){
         block.paused = false;
       },
-      
+
       pages: function(){
         return block.element.find('.page');
       },
-      
+
       removePage: function(id){
         var page = block.pages().filter('#promo_block_page_' + id).first();
-        
+
         if(page.hasClass('current')){
           block.rotate();
         }
-        
+
         page.remove();
       },
 
