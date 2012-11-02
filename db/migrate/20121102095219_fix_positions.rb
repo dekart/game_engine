7f43c48076e239a45ecc033d992928d30a5c50eb
olga <ahmadeeva.ou@gmail.com>
2012-11-02 17:53:30 +0600

    Migration fixed

--- db/migrate/20121102095219_fix_positions.rb
+++ db/migrate/20121102095219_fix_positions.rb
@@ -1,12 +1,30 @@
 class FixPositions < ActiveRecord::Migration
   def up
-    [ItemGroup, MissionGroup, Mission].each do |klass|
-
+    [ItemGroup, MissionGroup].each do |klass|
       klass.with_state(:deleted).select{|obj| obj.position}.each do |obj|
-        klass.update_all(
-          "position = (position - 1)", "state != \'deleted\' AND position > #{obj.position}"
-        )
         obj.update_attribute(:position, nil)
+      end
+
+      objects = klass.without_state(:deleted).sort_by{|obj| obj.position }
+      i = 1
+
+      objects.each do |obj|
+        obj.update_attribute(:position, i)
+        i += 1
+      end
+    end
+
+    Mission.with_state(:deleted).select{|obj| obj.position}.each do |obj|
+      obj.update_attribute(:position, nil)
+    end
+
+    MissionGroup.without_state(:deleted).each do |group|
+      missions = group.missions.without_state(:deleted).sort_by{|obj| obj.position }
+      i = 1
+
+      missions.each do |obj|
+        obj.update_attribute(:position, i)
+        i += 1
       end
     end
   end
