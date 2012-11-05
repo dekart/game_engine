00c23fd41d7d869e7781d712731063a402c81253
DEkart <rene.dekart@gmail.com>
2012-11-05 16:04:48 +0500

    Forgot to add files

---
+++ vendor/assets/javascripts/jquery-ui/all.js
@@ -0,0 +1,7 @@
+//= require jquery-ui/core
+//= require jquery-ui/widget
+//= require jquery-ui/mouse
+//= require jquery-ui/position
+//= require jquery-ui/draggable
+//= require jquery-ui/droppable
+//= require jquery-ui/tooltip
