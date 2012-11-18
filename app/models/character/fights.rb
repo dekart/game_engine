634539219076c79a30399fbe47e3fd9a6293a5ae
DEkart <rene.dekart@gmail.com>
2012-11-18 14:32:41 +0500

    Spaces

--- app/models/character/fights.rb
+++ app/models/character/fights.rb
@@ -18,29 +18,29 @@ class Character
         before_save   :update_fight_availability_time, :if => :hp_changed?
       end
     end
-    
+
     def fight_requirements
       @requirements ||= Requirements::Collection.new(
         weakness_requirement,
         Requirements::StaminaPoint.new(:value => Setting.i(:fight_stamina_required))
       )
     end
-    
+
     protected
 
     def update_fight_availability_time
-      if !Setting.b(:fight_weak_opponents) && weak? 
+      if !Setting.b(:fight_weak_opponents) && weak?
         Fight::OpponentBuckets.delete(self) # Removing player from opponent list
-        
+
         self.fighting_available_at = hp_restore_time(weakness_minimum).seconds.from_now
       end
 
       true
     end
-    
+
     def update_opponent_bucket
       Fight::OpponentBuckets.update(self)
-      
+
       true
     end
   end
