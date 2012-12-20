0f60bbb167bd6bdef3a7aa8a617aa66c9b58cad1
olga <ahmadeeva.ou@gmail.com>
2012-12-20 14:01:57 +0600

    Batch api for app_requests

--- lib/jobs/request_delete.rb
+++ lib/jobs/request_delete.rb
@@ -1,8 +1,21 @@
 module Jobs
-  class RequestDelete < Struct.new(:request_ids)
+  class RequestDelete
     def perform
-      AppRequest::Base.find_all_by_id(request_ids).each do |request|
-        request.delete_from_facebook!
+      request_ids = $redis.smembers("app_requests_for_deletion")
+
+      request_ids.each_slice(50) do |ids|
+        requests = AppRequest::Base.find_all_by_id(ids)
+        graph_api_ids = requests.collect{|request| request.graph_api_id }
+
+        batch_result = AppRequest::Base.delete_from_facebook!(graph_api_ids)
+
+        batch_result.each_with_index do |result, index|
+          if result == true
+            $redis.srem("app_requests_for_deletion", requests[index].id)
+          else
+            requests[index].mark_broken! if requests[index].can_mark_broken?
+          end
+        end
       end
     end
   end
