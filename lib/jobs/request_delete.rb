7fc1a3b9db281b03e412530541f592ebd25c66c7
olga <ahmadeeva.ou@gmail.com>
2013-01-11 16:49:38 +0600

    Reduce the batch size (40)

--- lib/jobs/request_delete.rb
+++ lib/jobs/request_delete.rb
@@ -2,16 +2,14 @@ module Jobs
   class RequestDelete
     def perform
       begin
-        request_ids = $redis.smembers("app_requests_for_deletion")[0..200]
+        request_ids = $redis.smembers("app_requests_for_deletion")[0..40]
 
         request_ids.each{|id| $redis.srem("app_requests_for_deletion", id)}
 
-        request_ids.each_slice(50) do |ids|
-          batch_result = AppRequest::Base.delete_from_facebook!(ids)
+        batch_result = AppRequest::Base.delete_from_facebook!(request_ids)
 
-          batch_result.each_with_index do |result, index|
-            $redis.sadd("app_requests_failed_deletion", ids[index]) unless result
-          end
+        batch_result.each_with_index do |result, index|
+          $redis.sadd("app_requests_failed_deletion", request_ids[index]) unless result
         end
       end while request_ids.size > 0
 
