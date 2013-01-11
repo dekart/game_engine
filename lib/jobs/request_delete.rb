b4468caece449512152b5318039b6700ef87e448
olga <ahmadeeva.ou@gmail.com>
2013-01-11 16:33:37 +0600

    App request batch api: fix

--- lib/jobs/request_delete.rb
+++ lib/jobs/request_delete.rb
@@ -1,17 +1,19 @@
 module Jobs
   class RequestDelete
     def perform
-      request_ids = $redis.smembers("app_requests_for_deletion")
+      begin
+        request_ids = $redis.smembers("app_requests_for_deletion")[0..200]
 
-      request_ids.each_slice(50) do |ids|
-        batch_result = AppRequest::Base.delete_from_facebook!(ids)
+        request_ids.each{|id| $redis.srem("app_requests_for_deletion", id)}
 
-        batch_result.each_with_index do |result, index|
-          $redis.srem("app_requests_for_deletion", ids[index])
+        request_ids.each_slice(50) do |ids|
+          batch_result = AppRequest::Base.delete_from_facebook!(ids)
 
-          $redis.sadd("app_requests_failed_deletion", ids[index]) unless result
+          batch_result.each_with_index do |result, index|
+            $redis.sadd("app_requests_failed_deletion", ids[index]) unless result
+          end
         end
-      end
+      end while request_ids.size > 0
 
       $redis.set("app_requests_last_processed_at", Time.now.to_i)
     end
