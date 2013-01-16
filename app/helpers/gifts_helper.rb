1105413fe4b02f898c77938592aecee80cf7ead1
DEkart <rene.dekart@gmail.com>
2013-01-17 01:28:00 +0500

    Move gift request text to app requests

--- app/helpers/gifts_helper.rb
+++ app/helpers/gifts_helper.rb
@@ -1,11 +1,11 @@
 module GiftsHelper
-  
+
   # Render button, which shows facebook dialog for sending gift to another players
   def gift_send_button(item, options = {})
     options = options.deep_merge(
       :dialog => {
-        :title    => t("gifts.new.request.title", :item => item.name),
-        :message  => t("gifts.new.request.message", :item => item.name, :app => t('app_name')),
+        :title    => t("app_requests.invites.gift.title", :item => item.name),
+        :message  => t("app_requests.invites.gift.text", :item => item.name, :app => t('app_name')),
         :data     => {
           :target_id => item.id,
           :target_type => Item.name
@@ -16,10 +16,10 @@ module GiftsHelper
         :target_type => Item.name
       }
     )
-    
-    link_to_function(button(:send), invite_dialog(:gift, options), 
+
+    link_to_function(button(:send), invite_dialog(:gift, options),
       :class => "send button"
     )
   end
-  
+
 end
