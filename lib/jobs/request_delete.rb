849ac124053a525d2180bc83bbed4472ee22e978
DEkart <rene.dekart@gmail.com>
2013-01-27 00:15:23 +0500

    Write request deletion timestamp before actual processing to avoid starting two jobs at once

--- lib/jobs/request_delete.rb
+++ lib/jobs/request_delete.rb
@@ -4,17 +4,15 @@ module Jobs
       return if $redis.get('app_requests_last_processed_at').to_i > 10.seconds.ago
 
       begin
+        $redis.set("app_requests_last_processed_at", Time.now.to_i)
+
         request_ids = $redis.smembers("app_requests_for_deletion")[0..40]
 
-        request_ids.each{|id| $redis.srem("app_requests_for_deletion", id)}
+        request_ids.each{|id| $redis.srem("app_requests_for_deletion", id) }
 
-        batch_result = AppRequest::Base.delete_from_facebook!(request_ids)
-
-        batch_result.each_with_index do |result, index|
+        AppRequest::Base.delete_from_facebook!(request_ids).each_with_index do |result, index|
           $redis.sadd("app_requests_failed_deletion", request_ids[index]) if result == false
         end
-
-        $redis.set("app_requests_last_processed_at", Time.now.to_i)
       end while request_ids.size > 0
     end
   end
